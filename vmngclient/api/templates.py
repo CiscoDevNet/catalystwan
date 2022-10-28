48af8cb6fa5a98f9e2208b6f528f1705f81106ca
tzietkow <tzietkow@cisco.com>
2022-10-27 22:01:46 -0700

    create class CreateCliTemplate

--- vmngclient/api/templates.py
+++ vmngclient/api/templates.py
@@ -1,117 +1,240 @@
-import json
 import logging
-import time
-from enum import Enum
-from typing import Dict, List, Union
+from typing import Dict, List, cast
 
-from attrs import frozen
+from ciscoconfparse import CiscoConfParse  # type: ignore
+from tenacity import retry, retry_if_result, stop_after_attempt, wait_fixed
 
-from vmngclient.dataclasses import DeviceInfo, TemplateData
+from vmngclient.dataclasses import DeviceInfo, Template
 from vmngclient.session import Session
 from vmngclient.utils.creation_tools import create_dataclass, get_logger_name
+from vmngclient.utils.operation_status import OperationStatus
+from vmngclient.utils.personality import Personality
 
 logger = logging.getLogger(get_logger_name(__name__))
 
 
 class TemplateNotFoundError(Exception):
+    """Used when a templete item is not found."""
+
     def __init__(self, template):
         self.message = f"No such template: '{template}'"
 
 
-class OperationStatus(Enum):
-    IN_PROGRESS = 'In progress'
-    FAILURE = 'Failure'
-    SUCCESS = 'Success'
+class TemplateAttached(Exception):
+    """Used when delete attached template."""
 
-
-@frozen
-class OperationInformation:
-    activity: List[str]
-    status: OperationStatus
+    def __init__(self, template):
+        self.message = f"Template: {template} is attached to device."
 
 
 class TemplateAPI:
+    """TemplateAPI."""
+
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    def get_template_list(self) -> List[TemplateData]:
-        data = self.session.get_data("/dataservice/template/device")
-        return [create_dataclass(TemplateData, template) for template in data]
+    @property
+    def templates(self) -> List[Template]:
+        """Templates.
 
-    def get_template_id(self, template_name: str) -> str:
-        templates = self.get_template_list()
-        for template in templates:
-            if template_name == template.name:
-                return template.template_id
-        raise TemplateNotFoundError(template_name)
+        Returns:
+            List[Template]: List exist template.
+        """
+        endpoint = "/dataservice/template/device"
+        data = self.session.get_data(endpoint)
+        return [create_dataclass(Template, template) for template in data]
 
-    def wait_until_operation_complete(self, operation_id: str, timeout: int = 300) -> bool:
-        operation_status = self.get_operation_status(operation_id)
-        # logger.debug(json.dumps(self.get_request_status(operation_id), indent=4))
-        logger.debug(operation_status)
-        while any(filter(lambda x: x == OperationStatus.IN_PROGRESS, operation_status)):
-            logger.debug("Operation(s) in progress.")
-            time.sleep(30)
-            operation_status = self.get_operation_status(operation_id)
-            logger.debug(operation_status)
-        # if operation_status == OperationStatus.SUCCESS:
-        #     return True
-        # elif operation_status == OperationStatus.FAILURE:
-        
+    def get(self, name: str) -> Template:
+        """Get template.
 
-        logger.debug(json.dumps(self.get_request_status(operation_id)['data'], indent=4))
-        return False
+        Args:
+            name (str): Name of template.
 
-    def tt(self):
+        Raises:
+            TemplateNotFoundError: If template is not exist.
+
+        Returns:
+            Template: Selected template.
+        """
+        for template in self.templates:
+            if name == template.name:
+                return template
+        raise TemplateNotFoundError(name)
+
+    def get_id(self, name: str) -> str:
+        """Get id template.
+
+        Args:
+            name (str): Name of template to get id.
+
+        Returns:
+            str: Template id.
+        """
+        return self.get(name).template_id
+
+    def wait_complete(self, operation_id: str, timeout_seconds: int = 300, sleep_seconds: int = 5) -> bool:
+        """Wait to complete action.
+
+        Args:
+            operation_id (str): Id of the action waiting for completion.
+            timeout_seconds (int, optional): Time for completion. Defaults to 300.
+            sleep_seconds (int, optional): Sleep time between repetitions. Defaults to 5.
+
+        Returns:
+            bool: True if acction status is successful, otherwise - False.
+        """
+
+        def check_status(action_data):
+            if action_data:
+                list_action = [action == OperationStatus.SUCCESS for action in action_data]
+                return not all(list_action)
+            else:
+                return True
+
+        @retry(
+            wait=wait_fixed(sleep_seconds),
+            stop=stop_after_attempt(int(timeout_seconds / sleep_seconds)),
+            retry=retry_if_result(check_status),
+        )
+        def wait_for_status():
+            return self.get_operation_status(operation_id)
+
+        return True if wait_for_status() else False
+
+    def attach(self, template_name: str, device: DeviceInfo) -> bool:
+        """Attach template to device.
+
+        Args:
+            template_name (str): Template name to attached.
+            device (DeviceInfo): Device to attach template.
+
+        Returns:
+            bool: True if attached template is successful, otherwise - False.
+        """
+        templateId = self.get_id(template_name)
         payload = {
-            "templateId": "",
-            "device": {
-                "csv-status": "",
-                "csv-deviceId": "",
-                "csv-deviceIP": "",
-                "csv-host-name": "",
-                "csv-templateId": "",
-            },
+            "deviceTemplateList": [
+                {
+                    "templateId": templateId,
+                    "device": [
+                        {
+                            "csv-status": "complete",
+                            "csv-deviceId": device.uuid,
+                            "csv-deviceIP": device.id,
+                            "csv-host-name": device.hostname,
+                            "csv-templateId": templateId,
+                        }
+                    ],
+                }
+            ]
         }
-
-        r = self.session.post(url="/dataservice/template/device/config/config", data=payload)
-        return r
-
-    def generate_device_input(self, template_id: str, device_id: str):
-        payload = {
-            "templateId": template_id,
-            "deviceIds": [device_id],
-        }
-        return self.session.post_data("/dataservice/template/device/config/input", data=payload)[0]
-
-    def attach_cli_template(self, template_ids, devices: Union[List[DeviceInfo], DeviceInfo]) -> str:
-        if isinstance(devices, DeviceInfo):
-            devices = list(devices)
-        devices_input = [
-            self.generate_device_input(template_id, device.uuid) for template_id, device in zip(template_ids, devices)
-        ]
-        device_template_list = [
-            {"templateId": template_id, "device": [device_input]}
-            for template_id, device_input in zip(template_ids, devices_input)
-        ]
-
         endpoint = "/dataservice/template/device/config/attachcli"
-        logger.debug(json.dumps(device_template_list, indent=4))
-        payload = {"deviceTemplateList": device_template_list}
-        request_id = self.session.post_json(endpoint, payload)
-        logger.debug(f"Request id: {request_id}")
-        return request_id['id']
+        response = cast(dict, self.session.post_json(url=endpoint, data=payload))
+        return self.wait_complete(response['id'])
+
+    def device_to_cli(self, device: DeviceInfo) -> bool:
+        """Device mode to Cli.
+
+        Args:
+            device (DeviceInfo): Device to chcange mode.
+
+        Returns:
+            bool: True if change mode to cli is successful, otherwise - False.
+        """
+        payload = {"deviceType": device.personality, "devices": [{"deviceId": device.uuid, "deviceIP": device.id}]}
+        endpoint = "/dataservice/template/config/device/mode/cli"
+        response = cast(dict, self.session.post_json(url=endpoint, data=payload))
+        return self.wait_complete(response['id'])
 
     def get_operation_information(self, id: str) -> List[Dict[str, str]]:
+        """Get operation information.
+
+        Args:
+            id (str): Operatrion id.
+
+        Returns:
+            List[Dict[str, str]]: List action status.
+        """
         endpoint = f"/dataservice/device/action/status/{id}"
-        return self.session.get_json(endpoint)
+        response = cast(list, self.session.get_data(endpoint))
+        return response
 
     def get_operation_status(self, operation_id: str) -> List[OperationStatus]:
-        return [OperationStatus(status['status']) for status in self.get_operation_information(operation_id)['data']]
+        """Get operatrion status.
 
-    def get(self, template_name: str) -> TemplateData:
-        templates = self.get_template_list()
-        for template in templates:
-            if template_name == template.name:
-                return template
-        raise TemplateNotFoundError(template_name)
+        Args:
+            operation_id (str): Operatrion id.
+
+        Returns:
+            List[OperationStatus]: List of operations.
+        """
+        return [OperationStatus(status['status']) for status in self.get_operation_information(operation_id)]
+
+    def delete(self, template_name: str) -> bool:
+        """Delete template.
+
+        Args:
+            template_name (str): Name template to delete.
+
+        Raises:
+            TemplateAttached: If template is attached to device.
+
+        Returns:
+            bool: True if deletion is successful, otherwise - False.
+        """
+        template = self.get(template_name)
+        endpoint = f"/dataservice/template/device/{template.template_id}"
+        if template.devices_attached == 0:
+            response = self.session.delete(url=endpoint)
+            return response.status == 200
+        raise TemplateAttached(template.name)
+
+
+class CreateCliTemplate:
+    """Create Cli Template."""
+
+    def __init__(self, session: Session, device_model: Personality, name: str, description: str) -> None:
+        self.session = session
+        self.device_model = device_model
+        self.name = name
+        self.description = description
+        self.config: CiscoConfParse = CiscoConfParse([])
+
+    def load_running(self, device: DeviceInfo) -> None:
+        """Load running config from device.
+
+        Args:
+            device (DeviceInfo): The device from which load config.
+        """
+        endpoint = f"/dataservice/template/config/running/{device.uuid}"
+        config = cast(dict, self.session.get_json(endpoint))
+        self.config = CiscoConfParse(config['config'].splitlines())
+
+    def send_to_vmanage(self) -> str:
+        """Send CLI template to vManage.
+
+        Returns:
+            str: Template id.
+        """
+        config_p = "\n".join(self.config.ioscfg)
+        payload = {
+            "templateName": self.name,
+            "templateDescription": self.description,
+            "deviceType": self.device_model.value,
+            "templateConfiguration": config_p,
+            "factoryDefault": False,
+            "configType": "file",
+        }
+        endpoint = "/dataservice/template/device/cli/"
+        response = cast(dict, self.session.post_json(url=endpoint, data=payload))
+        return response['templateId']
+
+    def add_to_config(self, add_config: CiscoConfParse, add_before: str) -> None:
+        """Adding config to exist config before existing value.
+
+        Args:
+            add_config (CiscoConfParse): Config to added.
+            add_before (str): The value before which to add config.
+        """
+        for comand in add_config:
+            self.config.ConfigObjs.insert_before(add_before, comand.ioscfg[0], atomic=True)
