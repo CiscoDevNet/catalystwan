ab8debd836e1c438df5c4ee4165e448a10326eae
danielui <daniel.bartnicki1123@gmail.com>
2022-10-28 12:10:35 +0200

    parametrize partition actions functions

--- vmngclient/api/partition_manager_api.py
+++ vmngclient/api/partition_manager_api.py
@@ -3,8 +3,10 @@ from vmngclient.session import Session
 from vmngclient.api.repository import Repository
 from vmngclient.dataclasses import DeviceInfo
 from typing import List
+from vmngclient.dataclasses import DeviceSoftwareVersions
 
 class PartitionManager:
+    #TODO add clearing version method
 
     def __init__(self, session: Session, devices : List[DeviceInfo],
                  vmanage_image : str = '') -> None:
@@ -12,7 +14,7 @@ class PartitionManager:
         self.vmanange_image = vmanage_image
         self.session = session
         self.devices = []
-        self.all_software_versions = Repository(self.session).get_all_versions()
+        self.devices_software_versions = Repository(self.session).get_devices_software_versions()
         
         for dev in devices:
             dev_dict = dict()
@@ -21,30 +23,37 @@ class PartitionManager:
             self.devices.append(dev_dict)
 
     def set_default_partition(self, version: str):
-
-        for dev in self.devices:
-            dev['version'] = self.all_software_versions[dev['deviceId']][version]
-            
-        print (self.devices)
+        
+        self.complete_device_list(version)
         url = '/dataservice/device/action/defaultpartition'
         payload = {'action': 'defaultpartition',
                    'devices': self.devices,
                    'deviceType': 'vmanage'
                   }
-
+        
         return self.session.post_json(url, payload)
 
-    def remove_available_partition(self):
+    def remove_partition(self, version: str):
         
-        for dev in self.devices:
-            dev['version'] = self.all_software_versions["availableVersions"]
-
+        self.complete_device_list(version)
         url = '/dataservice/device/action/removepartition'
         payload = {'action': 'removepartition',
                    'devices': self.devices,
                    'deviceType': 'vmanage'
                    }
-        return self.session.post_data(url, payload)
+
+        return self.session.post_json(url, payload)
     
+    def complete_device_list(self, version : str):
+        
+        for dev in self.devices:
+            for device in self.devices_software_versions:
+                if device.device_id == dev['deviceId']:
+                    dev['version'] = getattr(device,version)
+                    break
+        return self.devices
+
+
+
 
         
