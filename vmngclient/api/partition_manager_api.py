ef5a2dc1499558b333e5c2e25b8ab0cbb75d057e
danielui <daniel.bartnicki1123@gmail.com>
2022-11-04 15:02:58 +0100

    createad generic methods for upgrade,activate controllers

--- vmngclient/api/partition_manager_api.py
+++ vmngclient/api/partition_manager_api.py
@@ -3,10 +3,13 @@ from vmngclient.session import Session
 from vmngclient.api.repository import Repository
 from vmngclient.dataclasses import DeviceInfo
 from typing import List
-from vmngclient.dataclasses import DeviceSoftwareVersions
+from tenacity import retry, retry_if_result, stop_after_attempt, wait_fixed
+import logging
+from vmngclient.utils.creation_tools import get_logger_name
+
+logger = logging.getLogger(get_logger_name(__name__))
 
 class PartitionManager:
-    #TODO add clearing version method
 
     def __init__(self, session: Session, devices : List[DeviceInfo],
                  vmanage_image : str = '') -> None:
@@ -14,7 +17,7 @@ class PartitionManager:
         self.vmanange_image = vmanage_image
         self.session = session
         self.devices = []
-        self.devices_software_versions = Repository(self.session).get_devices_software_versions()
+        self.devices_software_versions = Repository(self.session, self.vmanange_image).get_devices_software_versions()
         
         for dev in devices:
             dev_dict = dict()
@@ -50,8 +53,36 @@ class PartitionManager:
             for device in self.devices_software_versions:
                 if device.device_id == dev['deviceId']:
                     dev['version'] = getattr(device,version)
+                    if isinstance(dev['version'],list):
+                        dev['version'] = dev['version'][0]
                     break
         return self.devices
+    
+    def wait_for_completed(self, 
+        sleep_seconds: int,timeout_seconds: int,
+        expected_status: str,
+        action_id : str):
+        
+        def check_status(action_data):
+            return not action_data == expected_status
+
+        @retry(
+            wait=wait_fixed(sleep_seconds),
+            stop=stop_after_attempt(int(timeout_seconds / sleep_seconds)),
+            retry=retry_if_result(check_status),
+        )
+        def wait_for_end_partition_action():
+            status_api = f'/dataservice/device/action/status/{action_id}'
+            
+            try:
+                action_data = self.session.get_data(f'{status_api}')[0]['status']
+                logger.debug(f"Status of action {action_id} is: {action_data}")
+            except IndexError:
+                action_data = ''
+
+            return action_data
+
+        wait_for_end_partition_action()
 
 
 
