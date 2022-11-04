41bb15182fad552d15385bb0325556a2e5ad606f
danielui <daniel.bartnicki1123@gmail.com>
2022-11-04 15:02:58 +0100

    createad generic methods for upgrade,activate controllers

--- vmngclient/api/repository.py
+++ vmngclient/api/repository.py
@@ -1,7 +1,8 @@
 import os
 from vmngclient.session import Session
 from vmngclient.utils.creation_tools import create_dataclass
-from vmngclient.dataclasses import SoftwareVersions
+from vmngclient.dataclasses import DeviceSoftwareVersions
+from typing import List
 
 class Repository:
 
@@ -11,12 +12,6 @@ class Repository:
     
     def get_image_version(self) -> str:
         
-        '''
-        The image name in several cases doesn't contain whole image version.
-        It's necessary to get the version from vManage which shows whole version.
-
-        @param image: the image name
-        '''
         version = ''
         url = '/dataservice/device/action/software/images?imageType=software'
         image_name = os.path.basename(self.vmanage_image)
@@ -27,30 +22,15 @@ class Repository:
                 break
         return version
    
-    def get_all_software_versions(self):
-        #Maybe create dataclass for that?
-        # url = '/dataservice/system/device/controllers'
-        
-        # devices_versions = dict()
-        # devices = self.session.get_data(url)
-        # for dev in devices:
-        #     versions_dict = dict()
-        #     versions_dict['availableVersions'] = [version for version in
-        #                                           dev['availableVersions']]
-        #     versions_dict['currentVersion'] = dev['version']
-        #     versions_dict['defaultVersion'] = dev['defaultVersion']
-        #     versions_dict['UpgradeVersion'] = self.get_image_version()
-        #     devices_versions[dev['uuid']] = versions_dict
-        
-        # return devices_versions
+    def get_devices_software_versions(self)-> List[DeviceSoftwareVersions] : 
 
         url = '/dataservice/system/device/controllers'
         devices = self.session.get_data(url)
         self.software_versions = []
-        upgrade_version = self.get_image_version()
+        image_version = self.get_image_version()
         for device in devices:
-            software_versions_object = create_dataclass(SoftwareVersions,device)
-            software_versions_object.upgrade_version = upgrade_version
+            software_versions_object = create_dataclass(DeviceSoftwareVersions,device)
+            software_versions_object.image_version = image_version
             self.software_versions.append(software_versions_object)
         return self.software_versions
 
