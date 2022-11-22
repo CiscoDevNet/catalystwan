639231017b044cb169b5a9510c54ec769b1e3d8b
Tomasz Warda <85410520+tomaszwarda@users.noreply.github.com>
2022-11-22 12:28:42 +0100

    Port as Optional parameter (#28)
    * port as option
    
    * mypy fix
    
    * unused import and unittest
    
    * fix

--- vmngclient/tests/test_session.py
+++ vmngclient/tests/test_session.py
@@ -6,6 +6,6 @@ from vmngclient.session import Session
 class TestSessionConstructor(unittest.TestCase):
     def test_session_str(self):
         # Arrange, Act
-        session = Session("1.1.1.1", 111, "admin", None)
+        session = Session("1.1.1.1", "admin", None, port=111)
         # Assert
         self.assertEqual(str(session), "admin@https://1.1.1.1:111")
