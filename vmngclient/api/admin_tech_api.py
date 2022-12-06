179f4c050fc78bd27e8bea85a325c3f3de678da4
JimOverholt <115191247+JimOverholt@users.noreply.github.com>
2022-12-06 03:19:32 -0800

    get_file (#47)
    * get_file
    
    * usage
    
    * fix mypy/pylance issues
    
    Co-authored-by: sbasan <sbasan@cisco.com>

--- vmngclient/api/admin_tech_api.py
+++ vmngclient/api/admin_tech_api.py
@@ -2,7 +2,6 @@
 Module for handling admintech logs for a device
 """
 import json
-import shutil
 import time
 from pathlib import Path
 from typing import List, Optional, cast
@@ -120,7 +119,6 @@ class AdminTechAPI:
         if not download_dir:
             download_dir = Path.cwd()
         download_path = download_dir / admin_tech_name
-        with self.session.get_data(f'/dataservice/device/tools/admintech/download/{admin_tech_name}') as payload:
-            with open(download_path, 'wb') as file:
-                shutil.copyfileobj(payload, file)
+        url = f'/dataservice/device/tools/admintech/download/{admin_tech_name}'
+        self.session.get_file(url, download_path)
         return download_path
