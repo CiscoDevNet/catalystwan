56806068516a00fde2fbc70d74cdf918eb40705d
tomaszwarda <tomeqqq99@gmail.com>
2022-12-13 13:21:21 +0100

    getLogger change

--- vmngclient/__init__.py
+++ vmngclient/__init__.py
@@ -5,7 +5,7 @@ from typing import Final
 
 LOGGING_CONF_DIR: Final[str] = str(Path(__file__).parents[0] / 'logging.conf')
 
-vmngclient_logger = logging.getLogger("vmngclient")
+vmngclient_logger = logging.getLogger(__name__)
 
 if not vmngclient_logger.handlers:
     logging.config.fileConfig(LOGGING_CONF_DIR, disable_existing_loggers=False)
