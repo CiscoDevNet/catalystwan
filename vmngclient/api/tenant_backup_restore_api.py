7a175ef572024c70715c60fc380f46fbdeefa155
Jim Overholt <overholt@cisco.com>
2022-12-27 18:01:31 -0800

    TaskStatus

--- vmngclient/api/tenant_backup_restore_api.py
+++ vmngclient/api/tenant_backup_restore_api.py
@@ -1,11 +1,12 @@
 """
-Module for handling tenant backup and restore
+Module for tenant backup and restore API
 """
 import logging
+import re
 from pathlib import Path
 from typing import Optional, Union
 
-# from vmngclient.api.task_status_api import TaskStatus
+from vmngclient.api.task_status_api import TaskStatus
 from vmngclient.dataclasses import TenantBackupRestore
 from vmngclient.session import vManageSession
 from vmngclient.utils.creation_tools import create_dataclass
@@ -22,16 +23,13 @@ class TenantBackupRestoreApi:
         session: logged in API client session
     """
 
-    exit_statuses = [OperationStatus.SUCCESS.value, OperationStatus.FAILURE.value]
-    exit_statuses_ids = [OperationStatusId.SUCCESS.value, OperationStatusId.FAILURE.value]
-
     def __init__(self, session: vManageSession) -> None:
         self.session = session
 
     def __str__(self) -> str:
         return str(self.session)
 
-    def list_backups(self):
+    def list_backups(self) -> TenantBackupRestore:
         """Returns a list of backup files stored on vManage
         tenant admin will see only his own tenant backupfiles
         provider will see all tenants backup files.
@@ -42,67 +40,63 @@ class TenantBackupRestoreApi:
         response = self.session.get_json("/dataservice/tenantbackup/list")
         return create_dataclass(TenantBackupRestore, response)
 
-    def export(self) -> str:
+    def export(self, timeout: int = 300) -> str:
         """Export tenant backup file
 
         Returns:
             filename on vManage server of exported tenant backup file
         """
         response = self.session.get_json("/dataservice/tenantbackup/export")
-        # task_status = TaskStatus(self.session)
-        # result = task_status.wait_for_completed(
-        #     5, 3000,
-        #     self.exit_statuses,
-        #     self.exit_statuses_ids,
-        #     response['processId'])
-        return str(response)
+        task_status = TaskStatus(self.session)
+        result = task_status.wait_for_completed( 5, response['processId'], timeout)
+        string = re.search("""file location: (.*)""", result.activity[-1])
+        assert string, "File locationa not found."
+        return string.group(1)
 
-    def delete(self, file_name) -> Union[dict, list]:
+    def delete(self, file: str) -> Union[dict, list]:
         """Delete tenant backup file
 
         Returns:
             http response for delete operation
         """
-
-        url = f"/dataservice/tenantbackup/delete?fileName={file_name}"
+        url = f"/dataservice/tenantbackup/delete?fileName={file}"
         return self.session.delete(url).json()
 
     def delete_all(self) -> Union[dict, list]:
         return self.delete("all")
 
-    def download(self, file_name: str, download_dir: Optional[Path] = None) -> Path:
+    def download(self, file: str, download_dir: Optional[Path] = None) -> Path:
         """Download tenant backup file
 
         Args:
-            file_name: name of tenant backup file
+            file: name of tenant backup file
             download_dir: download directory (defaults to current working directory)
         Returns:
             path to downloaded tenant backup file
         """
 
-        download_dir = download_dir if download_dir else Path.cwd()
-        download_path = download_dir / file_name
-        tenant_id = file_name.split("_")[1]
-        url = f"/dataservice/tenantbackup/download/{tenant_id}/{file_name}"
-        self.session.get_file(url, download_path)
-        return download_path
+        if "/dataservice/tenantbackup/download/" not in file:
+            tenant_id = file.split("_")[1]
+            file = f"/dataservice/tenantbackup/download/{tenant_id}/{file}"
 
-    def import_backup(self, filename: Path):
+        download_dir = download_dir if download_dir else Path.cwd()
+        download = download_dir / Path(file).name
+        self.session.get_file(file, download)
+        return download
+
+    def import_backup(self, file: Path, timeout: int = 300) -> str:
         """
         upload the specified file for tenant import,
         then poll the task until success or failure.
         Args:
-            filename: The path of the file to be upladed
+            file: The path of the file to be upladed
         Returns:
         """
-        # Upload the file
+
         url = "/dataservice/tenantbackup/import"
-        files = {"file": (filename.name, open(str(filename), "rb"))}
+        files = {"file": (file.name, open(str(file), "rb"))}
         response = self.session.post(url, data={}, files=files)
-        # task_status = TaskStatus(self.session)
-        # result = task_status.wait_for_completed(
-        #     5, 3000,
-        #     self.exit_statuses,
-        #     self.exit_statuses_ids,
-        #     response.json()['processId'])
-        return response
+
+        task_status = TaskStatus(self.session)
+        result = task_status.wait_for_completed( 5, response.json()['processId'], timeout)
+        return result
