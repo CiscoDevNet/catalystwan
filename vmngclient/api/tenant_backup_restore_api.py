3a037849940113969f6ddeaf01db99393a4e1ef8
Jim Overholt <overholt@cisco.com>
2022-12-29 12:01:11 -0800

    docstring

--- vmngclient/api/tenant_backup_restore_api.py
+++ vmngclient/api/tenant_backup_restore_api.py
@@ -4,7 +4,7 @@ Module for tenant backup and restore API
 import logging
 import re
 from pathlib import Path
-from typing import Optional, Union
+from typing import List, Optional, Union
 
 from vmngclient.api.task_status_api import TaskStatus
 from vmngclient.dataclasses import TenantBackupRestore
@@ -16,34 +16,61 @@ logger = logging.getLogger(__name__)
 
 class TenantBackupRestoreApi:
     """
-    Class for handling tenant backup and restore
+    Class for tenant backup and restore
+
+    Scope:
+        Provider as tenant, tenant.
 
     Attributes:
-        session: logged in API client session
+        session (vManageSession): logged in API client session
+
+    Example usage:
+        from vmngclient.api.tenant_backup_restore_api import TenantBackupRestoreApi
+        from vmngclient.session import create_vManageSession
+
+        ProviderAsTenantSession = create_vManageSession(
+            domain, admin, password, port, subdomain=tenantDomain)
+        TenantSession = create_vManageSession(
+             tenantDomain, tenantadmin, password, port)
+
+        TenantBackupRestore = TenantBackupRestoreApi(TenantSession)
+        ProviderBackupRestore = TenantBackupRestoreApi(ProviderAsTenantSession)
+
+        fileName = TenantBackupRestore.export()
+        fileList = ProviderBackupRestore.list()
+        file = TenantBackupRestore.download(fileName)
+        status = ProviderBackupRestore.import_db(file)
+        deletedList = TenantBackupRestore.delete(fileName)
+        deletedList = ProviderBackupRestore.delete_all()
+
     """
 
     def __init__(self, session: vManageSession) -> None:
         self.session = session
 
-    def __str__(self) -> str:
-        return str(self.session)
-
-    def list_backups(self) -> TenantBackupRestore:
-        """Returns a list of backup files stored on vManage
-        tenant admin will see only his own tenant backupfiles
-        provider will see all tenants backup files.
+    def list(self) -> TenantBackupRestore:
+        """Return a list of backup files stored on vManage.
 
         Returns:
             list of filenames
+
+        Example usage:
+            fileList = ProviderBackupRestore.list()
         """
         response = self.session.get_json("/dataservice/tenantbackup/list")
         return create_dataclass(TenantBackupRestore, response)
 
     def export(self, timeout: int = 300) -> str:
-        """Export tenant backup file
+        """Export tenant backup file from DB to vManage storage.
+
+        Args:
+            timeout: Max polling time for task (default: 300 seconds).
 
         Returns:
-            filename on vManage server of exported tenant backup file
+            str: filename of exported tenant backup file stored on vManage.
+
+        Example usage:
+            fileName = ProviderBackupRestore.export()
         """
         response = self.session.get_json("/dataservice/tenantbackup/export")
         task_status = TaskStatus(self.session)
@@ -52,26 +79,44 @@ class TenantBackupRestoreApi:
         assert string, "File locationa not found."
         return string.group(1)
 
-    def delete(self, file: str) -> Union[dict, list]:
+    def delete(self, file: str) -> List[str]:
         """Delete tenant backup file
+
+        Args:
+            file (str): device ID (usually system-ip)
 
         Returns:
             http response for delete operation
+
+        Example usage:
+            message = ProviderBackupRestore.delete(fileName)
         """
         url = f"/dataservice/tenantbackup/delete?fileName={file}"
-        return self.session.delete(url).json()
+        return self.session.delete(url).json()['Deleted']
 
     def delete_all(self) -> Union[dict, list]:
+        """Delete all tenant backup file
+
+        Returns:
+            http response for delete operation
+
+        Example usage:
+            message = ProviderBackupRestore.delete_all()
+        """
         return self.delete("all")
 
     def download(self, file: str, download_dir: Optional[Path] = None) -> Path:
         """Download tenant backup file
-
+ 
         Args:
-            file: name of tenant backup file
-            download_dir: download directory (defaults to current working directory)
+            file: full path or base name of tenant backup file
+            download_dir: download directory (defaul: current working directory)
+
         Returns:
-            path to downloaded tenant backup file
+            Path to downloaded tenant backup file
+
+        Example usage:
+            file = ProviderBackupRestore.download(fileName)
         """
 
         if "/dataservice/tenantbackup/download/" not in file:
@@ -83,13 +128,18 @@ class TenantBackupRestoreApi:
         self.session.get_file(file, download)
         return download
 
-    def import_backup(self, file: Path, timeout: int = 300) -> TaskStatus:
-        """
-        upload the specified file for tenant import,
-        then poll the task until success or failure.
+    def import_db(self, file: Path, timeout: int = 300) -> TaskStatus:
+        """Upload the file for tenant import to the DB and poll for Success.
+
         Args:
             file: The path of the file to be upladed
+            timeout: Max polling time for task (default: 300 seconds).
+
         Returns:
+            TaskStatus object with Success and Activity messages.
+
+        Example usage:
+             status = ProviderBackupRestore.import_db(file)
         """
 
         url = "/dataservice/tenantbackup/import"
@@ -97,5 +147,5 @@ class TenantBackupRestoreApi:
         response = self.session.post(url, data={}, files=files)
 
         task_status = TaskStatus(self.session)
-        result = task_status.wait_for_completed(response.json()['processId"], timeout, 5)
+        result = task_status.wait_for_completed(response.json()["processId"], timeout, 5)
         return result
