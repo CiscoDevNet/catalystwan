6168242c7cb116b4dc03e01f4c231b9984dc9ecc
danielui <daniel.bartnicki1123@gmail.com>
2023-01-04 14:48:40 +0100

    use dataclass for wait_for_completed

--- vmngclient/api/task_status_api.py
+++ vmngclient/api/task_status_api.py
@@ -6,117 +6,100 @@ from tenacity import retry, retry_if_result, stop_after_attempt, wait_fixed  # t
 from vmngclient.session import vManageSession
 from vmngclient.utils.creation_tools import get_logger_name
 from vmngclient.utils.operation_status import OperationStatus, OperationStatusId
+from attr import define, field
+from vmngclient.utils.creation_tools import FIELD_NAME
+from vmngclient.utils.creation_tools import create_dataclass
 
 logger = logging.getLogger(get_logger_name(__name__))
 
-
+@define
 class TaskStatus:
+    status: str
+    status_id: str = field(metadata={FIELD_NAME: "statusId"})
+    activity: List[str]
+
+def wait_for_completed(
+    session: vManageSession,
+    action_id: str,
+    timeout_seconds: int = 300,
+    sleep_seconds: int = 5,
+    exit_statuses: List[OperationStatus] = [
+        OperationStatus.SUCCESS,
+        OperationStatus.FAILURE,
+    ],
+    exit_statuses_ids: List[OperationStatusId] = [
+        OperationStatusId.SUCCESS,
+        OperationStatusId.FAILURE,
+    ],
+    activity_text: str = '',
+) -> list:
     """
-    API class to check task status
-        Example usage:
+    Method to check action status
 
-        session = create_vManageSession(ip_address,admin_username,password,port=port)
-        devices = DevicesAPI(session).devices
-        vsmart_device = [dev for dev in devices if dev.personality == Personality.VSMART][0]
-        exit_statuses = [OperationStatus.SUCCESS.value, OperationStatus.FAILURE.value]
-        exit_statuses_ids = [OperationStatusId.SUCCESS.value, OperationStatusId.FAILURE.value]
+    Args:
+        sleep_seconds (int): interval between action status requests
+        timeout_seconds (int): After this time, function will stop requesting action status
+        exit_statuses (Union[List[OperationStatus], str]): actions statuses that cause stop requesting action status
+        exit_statuses_ids (Union[List[OperationStatusId], str]): actions statuses ids
+            that cause stop requesting action status id
+        action_id (str): inspected action id
+        activity_text (str): activity text
+        action_url (str, optional): Action url. Defaults to '/dataservice/device/action/status/'
 
-        reboot_action = RebootAction(session,devices)
-        reboot_action.execute()
-
-        # Keep asking for reboot status until it's not in exit_statuses (Failure or Success)
-        # or timeout is not achieved (3000s)
-        TaskStatus(session).wait_for_completed(5,3000,exit_statuses,exit_statuses_ids,reboot_action.action_id)
-
+    Returns:
+        "TaskStatus": 
     """
+    action_url = '/dataservice/device/action/status/'
+    exit_statuses = [cast(OperationStatus, exit_status.value) for exit_status in exit_statuses]
+    exit_statuses_ids = [cast(OperationStatusId, exit_status_id.value) for exit_status_id in exit_statuses_ids]
 
-    def __init__(self, session: vManageSession) -> None:
-        self.session = session
-        self.status: str = ''
-        self.status_id: str = ''
-        self.activity: List[str] = []
-
-    def wait_for_completed(
-        self,
-        action_id: str,
-        timeout_seconds: int = 300,
-        sleep_seconds: int = 5,
-        exit_statuses: List[OperationStatus] = [
-            OperationStatus.SUCCESS,
-            OperationStatus.FAILURE,
-        ],
-        exit_statuses_ids: List[OperationStatusId] = [
-            OperationStatusId.SUCCESS,
-            OperationStatusId.FAILURE,
-        ],
-        activity_text: str = '',
-    ) -> "TaskStatus":
+    def check_status(task: TaskStatus) -> bool:
         """
-        Method to check action status
+        Function checks if condition is met. If so,
+        wait_for_completed stops asking for task status
 
         Args:
-            sleep_seconds (int): interval between action status requests
-            timeout_seconds (int): After this time, function will stop requesting action status
-            exit_statuses (Union[List[OperationStatus], str]): actions statuses that cause stop requesting action status
-            exit_statuses_ids (Union[List[OperationStatusId], str]): actions statuses ids
-             that cause stop requesting action status id
-            action_id (str): inspected action id
-            activity_text (str): activity text
-            action_url (str, optional): Action url. Defaults to '/dataservice/device/action/status/'
+            status (str): status of task
+            status_id (str): status id of task
+            activity (str): activity text
 
         Returns:
-            "TaskStatus": 
+            bool: False if condition is met
         """
-        action_url = '/dataservice/device/action/status/'
-        exit_statuses = [cast(OperationStatus, exit_status.value) for exit_status in exit_statuses]
-        exit_statuses_ids = [cast(OperationStatusId, exit_status_id.value) for exit_status_id in exit_statuses_ids]
 
-        def check_status(action_data) -> bool:
-            """
-            Function checks if condition is met. If so,
-            wait_for_completed stops asking for task status
+        if (task.status in exit_statuses) and (task.status_id in exit_statuses_ids):
+            if not activity_text or activity_text in task.activity:
+                return False
+        return True
 
-            Args:
-                status (str): status of task
-                status_id (str): status id of task
-                activity (str): activity text
+    def _log_exception() -> None:
+        logger.error("Operation status not achieved in given time")
 
-            Returns:
-                bool: False if condition is met
-            """
+    @retry(
+        wait=wait_fixed(sleep_seconds),
+        stop=stop_after_attempt(int(timeout_seconds / sleep_seconds)),
+        retry=retry_if_result(check_status),
+        retry_error_callback=_log_exception,
+    )
+    def wait_for_action_finish() -> list:
+        """
+        Keep asking for task status, status_id,
+        activity(optional), utill check_status is True
 
-            if (self.status in exit_statuses) and (self.status_id in exit_statuses_ids):
-                if not activity_text or activity_text in self.activity:
-                    return False
-            return True
-
-        def _log_exception(self) -> None:
-            logger.error("Operation status not achieved in given time")
-
-        @retry(
-            wait=wait_fixed(sleep_seconds),
-            stop=stop_after_attempt(int(timeout_seconds / sleep_seconds)),
-            retry=retry_if_result(check_status),
-            retry_error_callback=_log_exception,
+        Returns:
+            TaskStatus: TaskStatus instance
+        """
+        url = f'{action_url}{action_id}'
+        action_data = session.get_data(url)[0]
+        # status = action_data['status']
+        # status_id = action_data['statusId']
+        # activity = action_data['activity']
+        task = create_dataclass(TaskStatus, action_data)
+        logger.debug(
+            f"Statuses of action {action_id} is: "
+            f"status: {task.status}, status_id: {task.status_id}, activity: {task.activity} "
         )
-        def wait_for_action_finish() -> list:
-            """
-            Keep asking for task status, status_id,
-            activity(optional), utill check_status is True
+        return task
 
-            Returns:
-                TaskStatus: TaskStatus instance
-            """
-            url = f'{action_url}{action_id}'
-            action_data = self.session.get_data(url)[0]
-            self.status = action_data['status']
-            self.status_id = action_data['statusId']
-            self.activity = action_data['activity']
-            logger.debug(
-                f"Statuses of action {action_id} is: "
-                f"status: {self.status}, status_id: {self.status_id}, activity: {self.activity} "
-            )
-            return action_data
-
-        wait_for_action_finish()
-        return self
+    return wait_for_action_finish()
+    
