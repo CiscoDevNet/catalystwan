8b8f4c96266bd3e667ba0698a3118a2ca0392456
danielui <daniel.bartnicki1123@gmail.com>
2023-01-05 10:43:47 +0100

    add test for task_status

--- vmngclient/tests/test_task_status_api.py
+++ vmngclient/tests/test_task_status_api.py
@@ -1,38 +1,29 @@
 import unittest
-from unittest.mock import Mock, patch
-from vmngclient.api.task_status_api import wait_for_completed, TaskStatus
-from tenacity import RetryError
+from unittest.mock import patch
+
+from vmngclient.api.task_status_api import TaskStatus, wait_for_completed
+
 
 class TestTaskStatusApi(unittest.TestCase):
-
     def setUp(self):
-        self.task = TaskStatus('Success', "success", [])
-        self.not_completed_task = TaskStatus("Other_status","other_status",[]) 
+        self.task = TaskStatus("Success", "success", [])
 
     @patch("vmngclient.session.vManageSession")
-    def test_wait_for_completed_success(self,mock_session):
+    def test_wait_for_completed_success(self, mock_session):
 
-        #Prepare mock data
-        mock_session.get_data.return_value = [{'status':'Success', "statusId": "success", "activity":[]}]
+        # Prepare mock data
+        mock_session.get_data.return_value = [{"status": "Success", "statusId": "success", "activity": []}]
 
-        #Assert
-        answer = wait_for_completed(mock_session,
-        'mock_action_id',3000,5)
-        self.assertEqual(answer, self.task, 'job status incorrect')
- 
+        # Assert
+        answer = wait_for_completed(mock_session, "mock_action_id", 3000, 5)
+        self.assertEqual(answer, self.task, "job status incorrect")
+
     @patch("vmngclient.session.vManageSession")
     def test_wait_for_completed_status_out_of_range(self, mock_session):
 
-        #Prepare mock data
-        mock_session.get_data.return_value = [{'status':'Other_status', "statusId": "other_status", "activity":[]}]
-    
-        #Assert
-        # self.assertRaises(
-        #     RetryError,
-        #     wait_for_completed,
-        # mock_session,
-        # 'mock_action_id',1,1
-        # 
-        answer = wait_for_completed(mock_session,
-        'mock_action_id',1,1)
-        self.assertEqual(answer, self.not_completed_task, 'job status incorrect')
+        # Prepare mock data
+        mock_session.get_data.return_value = [{"status": "Other_status", "statusId": "other_status", "activity": []}]
+
+        # Assert
+        answer = wait_for_completed(mock_session, "mock_action_id", 1, 1)
+        self.assertEqual(answer, None, "job status incorrect")
