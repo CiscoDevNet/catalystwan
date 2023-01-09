517dfd1edaaad921a5b5df4e88d81ad2865add91
danielui <58788881+danielui@users.noreply.github.com>
2023-01-09 12:28:43 +0100

    Get task status generic method (#49)
    * first commit
    
    * changes
    
    * change
    
    * prepared return statement
    
    * add log exception
    
    * add operation status ids
    
    * add docstrings
    
    * pre-commit changes
    
    * pre-commit changes
    
    * fix return statement
    
    * fix return statemnt
    
    * delete prints
    
    * pre-commit fix
    
    * add usage example to docstring
    
    * Add operations status to deafult value
    
    * wait_for_completed return self
    
    * rm print
    
    * rm task endpoint as function argument
    
    * add default timeout in wait_for_completed
    
    * add done_scheduled operation status
    
    * add operation statuses
    
    * rm return None statement from log_excepction
    
    * fix DS in wait_for_completed
    
    * Set sleep_seconds def value = 5
    
    * change doc string
    
    * use dataclass for wait_for_completed
    
    * final version
    
    * pre-commit changes
    
    * change to double quotes
    
    * fix log exception
    add unitest for task status
    
    * add test for task_status
    
    * Update vmngclient/api/task_status_api.py
    
    Co-authored-by: Tomasz Warda <85410520+tomaszwarda@users.noreply.github.com>
    
    * add example to docstring
    
    * bump version
    
    Co-authored-by: Tomasz Warda <85410520+tomaszwarda@users.noreply.github.com>

--- vmngclient/utils/operation_status.py
+++ vmngclient/utils/operation_status.py
@@ -6,3 +6,14 @@ class OperationStatus(Enum):
     FAILURE = "Failure"
     SUCCESS = "Success"
     SCHEDULED = "Scheduled"
+    DONE_SCHEDULED = "Done - Scheduled"
+    VALIDATION_SUCCESS = "Validation success"
+
+
+class OperationStatusId(Enum):
+    IN_PROGRESS = "in_progress"
+    FAILURE = "failure"
+    SUCCESS = "success"
+    SCHEDULED = "scheduled"
+    DONE_SCHEDULED = "done_scheduled"
+    VALIDATION_SUCCESS = "validation_success"
