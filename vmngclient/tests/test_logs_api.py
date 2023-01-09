096746f0348a332b657ffad4ca3e1c7ac2d3621a
tomaszwarda <tomeqqq99@gmail.com>
2023-01-09 14:05:59 +0100

    parametrize

--- vmngclient/tests/test_logs_api.py
+++ vmngclient/tests/test_logs_api.py
@@ -2,6 +2,8 @@ import unittest
 from pathlib import Path
 from unittest.mock import patch
 
+from parameterized import parameterized  # type: ignore
+
 from vmngclient.api.logs_api import LogsAPI
 
 
@@ -23,13 +25,14 @@ class TestLogsAPI(unittest.TestCase):
         # Assert
         self.assertTrue(does_file_exist)
 
+    @parameterized.expand([["test_file.log"], ["other_test_file.log"]])
     @patch("vmngclient.session.vManageSession")
-    def test_get_auditlogs_file_path_provided(self, mock_session):
+    def test_get_auditlogs_file_path(self, file_name, mock_session):
         # Arrange
         mock_session.get_data.return_value = self.logs
-        file_path = Path(__file__).parents[0].joinpath("test_file.log")
+        file_path = Path(__file__).parents[2].joinpath(file_name)
         # Act
-        LogsAPI(mock_session).get_auditlogs(file_path=str(file_path))
+        LogsAPI(mock_session).get_auditlogs(file_path=file_name)
         does_file_exist = file_path.is_file()
         # Assert
         self.assertTrue(does_file_exist)
