73695bff6105eef029bf4a471da7a916d592781f
tomaszwarda <tomeqqq99@gmail.com>
2023-02-09 13:26:22 +0100

    certificate transformer

--- vmngclient/utils/creation_tools.py
+++ vmngclient/utils/creation_tools.py
@@ -118,3 +118,15 @@ def flatten_dict(d: Dict) -> Dict:
             yield key, value
 
     return dict(recurse(None, d))
+
+
+def certificate_field_transformer(cls, fields):
+    results = []
+    for field in fields:
+        if field.name == "retrieve_interval":
+            results.append(field)
+        elif isinstance(Enum, type(field)):
+            results.append(field.value)
+        else:
+            results.append(field)
+    return results
