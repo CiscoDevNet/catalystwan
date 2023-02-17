6a581eb3f6c80713ff9d079bc20222e3f00cdd9c
tehAgitto <kamil.gorski13@gmail.com>
2023-02-17 11:04:51 +0100

    Fix CiscoSystemModel.

--- vmngclient/api/templates/payloads/cisco_system/cisco_system_model.py
+++ vmngclient/api/templates/payloads/cisco_system/cisco_system_model.py
@@ -4,32 +4,17 @@ from __future__ import annotations
 from pathlib import Path
 from typing import ClassVar
 
-from attr import define  # type: ignore
-
 from vmngclient.api.templates.feature_template import FeatureTemplate
-from vmngclient.session import vManageSession
-
-
-@define
-class System:
-    host_name: str
-    system_ip: str
-    site_id: str
-    multi_tenant: bool
-    console_baud_rate: int
 
 
 class CiscoSystemModel(FeatureTemplate):
-    host_name: str
+    hostname: str
     system_ip: str
     site_id: str
-    multi_tenant: bool
+    multitenant: bool
     console_baud_rate: int
 
     class Config:
         arbitrary_types_allowed = True
 
     payload_path: ClassVar[Path] = Path(__file__).parent / "feature/cisco_system.json.j2"
-
-    def generate_payload(self, session: vManageSession) -> str:
-        return super().generate_payload(session)
