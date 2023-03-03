23deb0f0a1af001b64207d86eaa3ada6e8b2d9ea
tehAgitto <kamil.gorski13@gmail.com>
2023-03-03 17:11:17 +0100

    Add AlreadyExistsError Exception.

--- vmngclient/exceptions.py
+++ vmngclient/exceptions.py
@@ -12,4 +12,11 @@ class VersionDeclarationError(Exception):
     """The exception that is thrown in one of two below cases.
     1. User passes software image and version, at the same time.
     2. User doesn't passes any of them."""
+
+    pass
+
+
+class AlreadyExistsError(Exception):
+    """Raised when an entity that we attempted to create already exists."""
+
     pass
