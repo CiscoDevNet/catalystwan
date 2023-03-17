8141be15ebbee01a33f795c21a691c21711bf6d5
sbasan <sbasan@cisco.com>
2023-03-17 16:56:12 +0100

    tests still needs work

--- vmngclient/tests/test_utils_ratelimit.py
+++ vmngclient/tests/test_utils_ratelimit.py
@@ -1,32 +1,38 @@
+import logging
+import time
 import unittest
 from ipaddress import ip_address
+from multiprocessing import set_start_method
+from multiprocessing.pool import Pool, ThreadPool
 from unittest.mock import patch
 
 from parameterized import parameterized  # type: ignore
 
 from vmngclient.utils.ratelimit import (
-    DEFAULT_MAX_REQUESTS_PER_MINUTE,
-    max_requests_per_minute_setting,
+    DEFAULT_MAX_REQUESTS_PER_SECOND,
+    max_requests_per_second_setting,
     on_request_throttle,
     ratelimit,
     ratelimiters,
     throttle,
 )
 
+logger = logging.getLogger(__name__)
+
 
 class TestUtilsRateLimit(unittest.TestCase):
     def setUp(self) -> None:
         global ratelimiter
         ratelimiters.clear()
-        ratelimit(DEFAULT_MAX_REQUESTS_PER_MINUTE)
+        ratelimit(DEFAULT_MAX_REQUESTS_PER_SECOND)
         return super().setUp()
 
     def test_default_ratelimiter(self):
-        ratelimit(max_requests_per_minute=100)
-        assert ratelimiters[None].max_requests_per_minute == 100
-        assert ratelimiters[None].min_interval == 60.0 / 100.0
-        ratelimiters[None].max_requests_per_minute = 700
-        assert ratelimiters[None].min_interval == 60.0 / 700.0
+        ratelimit(max_requests_per_second=100)
+        assert ratelimiters[None].max_requests_per_second == 100
+        assert ratelimiters[None].min_interval == 1 / 100.0
+        ratelimiters[None].max_requests_per_second = 700
+        assert ratelimiters[None].min_interval == 1 / 700.0
 
     @parameterized.expand(
         [
@@ -40,8 +46,8 @@ class TestUtilsRateLimit(unittest.TestCase):
     @patch("socket.gethostbyaddr")
     def test_create_per_host_ratelimiters(self, rate, ip, mock_gethostbyaddr):
         mock_gethostbyaddr.return_value = str(ip)
-        ratelimit(max_requests_per_minute=rate, host=ip)
-        assert ratelimiters[ip].max_requests_per_minute == rate
+        ratelimit(max_requests_per_second=rate, host=ip)
+        assert ratelimiters[ip].max_requests_per_second == rate
 
     @parameterized.expand(
         [
@@ -56,7 +62,7 @@ class TestUtilsRateLimit(unittest.TestCase):
     def test_throttle_with_default_rate(self, ip, mock_gethostbyaddr):
         mock_gethostbyaddr.return_value = str(ip)
         throttle(host=ip)
-        assert ratelimiters[ip].max_requests_per_minute == max_requests_per_minute_setting
+        assert ratelimiters[ip].max_requests_per_second == max_requests_per_second_setting
 
     @parameterized.expand(
         [
@@ -70,6 +76,31 @@ class TestUtilsRateLimit(unittest.TestCase):
     @patch("socket.gethostbyaddr")
     def test_on_request_throttle_preset_rate(self, rate, url, ip, mock_gethostbyaddr):
         mock_gethostbyaddr.return_value = str(ip)
-        ratelimit(max_requests_per_minute=rate, host=ip)
+        ratelimit(max_requests_per_second=rate, host=ip)
         on_request_throttle(method="get", url=url)
-        assert ratelimiters[ip].max_requests_per_minute == rate
+        assert ratelimiters[ip].max_requests_per_second == rate
+
+    @patch("socket.gethostbyaddr")
+    def test_throttle_multiple_threads_and_processes(self, mock_gethostbyaddr):
+        # Given
+        ip = ip_address("127.0.0.2")
+        mock_gethostbyaddr.return_value = str(ip)
+        mp_items = [ip] * 3
+        t_items = [ip] * 4
+        item_count = len(mp_items) + len(t_items)
+        max_rps = 9
+        ratelimit(max_requests_per_second=max_rps)
+        min_expected_time_to_finish_tasks = item_count * 1.0 / max_rps
+        set_start_method("fork")
+        # Act
+        start = time.monotonic()
+        mp_results = Pool(len(mp_items)).map_async(throttle, mp_items)
+        t_results = ThreadPool(len(t_items)).map_async(throttle, t_items)
+        mp_results.wait()
+        t_results.wait()
+        finish = time.monotonic()
+        delta = finish - start
+        assert finish - start > min_expected_time_to_finish_tasks, (
+            f"{item_count} tasks finished in {delta:.2f}s, "
+            f"but expected no earlier than: {min_expected_time_to_finish_tasks:.2f}s for given rate {max_rps}/s"
+        )
--- vmngclient/utils/ratelimit.py
+++ vmngclient/utils/ratelimit.py
@@ -2,7 +2,7 @@
 This is utility module which can be used to limit requests rate globally (from multiple sessions, threads and processes)
 To enable ratelimiting for all requests sent from vmanage-client in your application:
 >>> from vmngclient.utils.ratelimit import ratelimit, on_request_throttle
->>> ratelimit(max_requests_per_minute=100)
+>>> ratelimit(max_requests_per_second=100)
 >>> vManageSession.on_request_hook = on_request_throttle
 >>> vManageAuth.on_request_hook = on_request_throttle
 Each unique host ip has own RateLimiter (host ip is obtained automatically from request url)
@@ -19,35 +19,37 @@ from typing import Dict, Union
 from urllib3.exceptions import LocationParseError, LocationValueError
 from urllib3.util import parse_url
 
-DEFAULT_MAX_REQUESTS_PER_MINUTE = 6000
-max_requests_per_minute_setting = DEFAULT_MAX_REQUESTS_PER_MINUTE
+DEFAULT_MAX_REQUESTS_PER_SECOND = 100  # according to:
+max_requests_per_second_setting = DEFAULT_MAX_REQUESTS_PER_SECOND
 logger = logging.getLogger(__name__)
+
+HostSpecifier = Union[IPv4Address, IPv6Address, None]
 
 
 class RateLimiter:
-    def __init__(self, max_requests_per_minute: int, host: Union[IPv4Address, IPv6Address, None]):
+    def __init__(self, max_requests_per_second: int, host: HostSpecifier):
         self.host = host
         self.tlock = threading.Lock()
         self.mplock = multiprocessing.Lock()
         self.last_request_timestamp = 0.0
-        self.max_requests_per_minute = max_requests_per_minute
+        self.max_requests_per_second = max_requests_per_second
         self.hostinfo = str(host) if host else "unknown"
 
     @property
-    def max_requests_per_minute(self):
-        return self._max_requests_per_minute
+    def max_requests_per_second(self):
+        return self._max_requests_per_second
 
-    @max_requests_per_minute.setter
-    def max_requests_per_minute(self, rpm: int):
-        self._max_requests_per_minute = rpm
-        self.min_interval = 60.0 / float(rpm)
+    @max_requests_per_second.setter
+    def max_requests_per_second(self, rps: int):
+        self._max_requests_per_second = rps
+        self.min_interval = 1.0 / float(rps)
         if self.host is None:
-            logger.info(f"Default ratelimiter set to {rpm} requests per minute")
+            logger.info(f"Default ratelimiter set to {rps} requests per second")
         else:
-            logger.info(f"Host: {self.host} limited to {rpm} requests per minute")
+            logger.info(f"Host: {self.host} limited to {rps} requests per second")
 
     def block(self, **kwargs):
-        # ensures that each call is separated with min_interval
+        # ensures that each call is separated with min_interval in seconds
         with self.tlock:
             with self.mplock:
                 elapsed = time.monotonic() - self.last_request_timestamp
@@ -58,10 +60,10 @@ class RateLimiter:
                 self.last_request_timestamp = time.monotonic()
 
 
-ratelimiters: Dict[Union[IPv4Address, IPv6Address, None], RateLimiter] = {}
+ratelimiters: Dict[HostSpecifier, RateLimiter] = {}
 
 
-def url_to_host(url: Union[str, bytes]) -> Union[IPv4Address, IPv6Address, None]:
+def url_to_host(url: Union[str, bytes]) -> HostSpecifier:
     """
     Takes url like "https://tenant1.domain.com:3333/dataservice/foo"
     Return primary host IP responding to the given url, returns None when host ip cannot be determined
@@ -79,23 +81,23 @@ def url_to_host(url: Union[str, bytes]) -> Union[IPv4Address, IPv6Address, None]
         return None
 
 
-def ratelimit(max_requests_per_minute: int, host: Union[IPv4Address, IPv6Address, None] = None):
+def ratelimit(max_requests_per_second: int, host: HostSpecifier = None):
     """
     Sets or changes ratelimit for given IP host
     If host not provided it changes default ratelimit setting for new ratelimiters
     RateLimiters are also created automatically by throttle function
     """
     global ratelimiters
-    global max_requests_per_minute_setting
+    global max_requests_per_second_setting
     if host is None:
-        max_requests_per_minute_setting = max_requests_per_minute
+        max_requests_per_second_setting = max_requests_per_second
     if not ratelimiters.get(host):
-        ratelimiters[host] = RateLimiter(max_requests_per_minute, host)
+        ratelimiters[host] = RateLimiter(max_requests_per_second, host)
     else:
-        ratelimiters[host].max_requests_per_minute = max_requests_per_minute
+        ratelimiters[host].max_requests_per_second = max_requests_per_second
 
 
-def throttle(host: Union[IPv4Address, IPv6Address, None], **kwargs):
+def throttle(host: HostSpecifier, **kwargs):
     """
     Ensures that each call is separated with min_interval for given Host
     Creates new Ratelimiter for given host if it does not exist
@@ -103,7 +105,7 @@ def throttle(host: Union[IPv4Address, IPv6Address, None], **kwargs):
     """
     global ratelimiters
     if not ratelimiters.get(host):
-        ratelimiters[host] = RateLimiter(max_requests_per_minute_setting, host)
+        ratelimiters[host] = RateLimiter(max_requests_per_second_setting, host)
     ratelimiters[host].block(**kwargs)
 
 
