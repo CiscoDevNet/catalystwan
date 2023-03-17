598f5b835b0aa4d2b1df822010298a41a9d664b4
sbasan <sbasan@cisco.com>
2023-03-17 11:05:48 +0100

    simplify

--- vmngclient/api/configuraton_disaster_recovery_api.py
+++ vmngclient/api/configuraton_disaster_recovery_api.py
@@ -17,12 +17,19 @@ class ClusterInfoDevice(DataclassBase):
 
 @define(kw_only=True)
 class ClusterInfo(DataclassBase):
-    primary: Optional[List[ClusterInfoDevice]] = field(
-        default=None, converter=lambda l: [create_dataclass(ClusterInfoDevice, info) for info in l]
-    )
-    secondary: Optional[List[ClusterInfoDevice]] = field(
-        default=None, converter=lambda l: [create_dataclass(ClusterInfoDevice, info) for info in l]
-    )
+    primary: Optional[List[ClusterInfoDevice]] = field(default=None)
+    secondary: Optional[List[ClusterInfoDevice]] = field(default=None)
+
+    def list_all_devices(self) -> List[ClusterInfoDevice]:
+        all_devices = []
+        if self.primary:
+            all_devices += self.primary
+        if self.secondary:
+            all_devices += self.secondary
+        return all_devices
+
+    def list_all_devices_ips(self) -> List[str]:
+        return [device.device_ip for device in self.list_all_devices()]
 
 
 class ConfigurationDisasterRecoveryApi:
@@ -32,33 +39,3 @@ class ConfigurationDisasterRecoveryApi:
     def get_cluster_info(self):
         response = self.session.get("dataservice/disasterrecovery/clusterInfo")
         return create_dataclass(ClusterInfo, response.payload.json.get("clusterInfo"))
-
-
-def test_nested_dataclass_parse():
-    json_payload = {
-        "clusterInfo": {
-            "primary": [
-                {
-                    "host-name": "vm12",
-                    "deviceIP": "10.0.105.32",
-                    "state": "UP",
-                    "isCurrentVManage": True,
-                }
-            ],
-            "secondary": [
-                {
-                    "host-name": "dc1-vm3",
-                    "deviceIP": "123.10.1.103",
-                    "state": "UP",
-                    "isCurrentVManage": True,
-                },
-                {
-                    "host-name": "dc1-vm2",
-                    "deviceIP": "123.10.1.102",
-                    "state": "UP",
-                    "isCurrentVManage": True,
-                },
-            ],
-        }
-    }
-    return create_dataclass(ClusterInfo, json_payload.get("clusterInfo"))
