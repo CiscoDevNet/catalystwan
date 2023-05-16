cc645950892c13605d7fbf8954452fa408dbbfea
tomaszwarda <tomeqqq99@gmail.com>
2023-05-16 14:07:10 +0200

    find_template_values

--- vmngclient/utils/feature_template.py
+++ vmngclient/utils/feature_template.py
@@ -1,4 +1,4 @@
-from typing import Type, TypeVar, Union
+from typing import Optional, Type, TypeVar, Union
 
 from vmngclient.api.templates.models.supported import FeatureTemplateType, supported_models
 from vmngclient.exceptions import TemplateTypeError
@@ -27,3 +27,25 @@ def choose_model(type_value: Union[str, FeatureTemplateType]) -> Type[T]:  # Is 
         if getattr(model, "type", None) == type_value:
             return model
     raise TemplateTypeError(f"Feature template type '{type_value}' is not supported.")
+
+
+def find_template_values(
+    data: dict,
+    final_list: list = [],
+    target_key: str = "vipType",
+    target_key_for_value: str = "vipValue",
+    parent_key: Optional[str] = None,
+):
+    for key, value in data.items():
+        if key == target_key and value != "ignore":
+            final_list.append((parent_key, data[target_key_for_value]))
+        elif isinstance(value, dict):
+            find_template_values(value, final_list, target_key, target_key_for_value, key)
+        elif isinstance(value, list) and key == target_key_for_value:
+            for item in value:
+                if isinstance(item, dict):
+                    final_list.append((parent_key, [find_template_values(item, [])]))
+    result = {}
+    for key, value in final_list:
+        result[key] = value
+    return result
