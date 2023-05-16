e528e06815ddcc7fd177256e494193165be90930
danielui <daniel.bartnicki1123@gmail.com>
2023-05-16 15:33:22 +0200

    change name in primitives

--- vmngclient/primitives/primitive_container.py
+++ vmngclient/primitives/primitive_container.py
@@ -18,4 +18,4 @@ class APIPrimitiveContainter:
         self.tenant_management = TenantManagementPrimitives(session)
         self.tenant_backup_restore = TenantBackupRestorePrimitives(session)
         self.tenant_migration = TenantMigrationPrimitives(session)
-        self.task_status = ConfigurationDashboardStatusPrimitives(session)
+        self.configuration_dashboard_status_primitives = ConfigurationDashboardStatusPrimitives(session)
