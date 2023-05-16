5211b437bf83a8e68d32a1283e6ba37ee0a023e2
danielui <daniel.bartnicki1123@gmail.com>
2023-05-16 14:09:31 +0200

    add keys

--- vmngclient/api/templates/models/system_vsmart_model.py
+++ vmngclient/api/templates/models/system_vsmart_model.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from typing import ClassVar
+from typing import ClassVar, Dict, List, Optional
+
+from pydantic import Field
 
 from vmngclient.api.templates.feature_template import FeatureTemplate
 
@@ -8,6 +10,14 @@ class SystemVsmart(FeatureTemplate):
     class Config:
         arbitrary_types_allowed = True
         allow_population_by_field_name = True
+
+    key: Optional[str]
+    details: Optional[str]
+    option_type: Optional[List[str]] = Field(default=None, alias="optionType")
+    default_option: Optional[str] = Field(default=None, alias="defaultOption")
+    data_type: Optional[Dict] = Field(default=None, alias="dataType")
+    data_path: Optional[List[str]] = Field(default=None, alias="dataPath")
+    object_type: Optional[str] = Field(default=None, alias="objectType")
 
     payload_path: ClassVar[Path] = Path(__file__).parent / "DEPRECATED"
     type: ClassVar[str] = "system-vsmart"
