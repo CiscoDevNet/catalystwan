c5b530879c17b064c5836705c820d7775c9c34a0
sbasan <sbasan@cisco.com>
2023-05-19 00:45:17 +0200

    add missing methods

--- vmngclient/api/tenant_migration_api.py
+++ vmngclient/api/tenant_migration_api.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
     from vmngclient.session import vManageSession
 
 
-def get_file_name_from_export_task_result(task_result: TaskResult) -> str:
+def _get_file_name_from_export_task_result(task_result: TaskResult) -> str:
     filepath = re.search("""file location: (.*)""", task_result.sub_tasks_data[0].activity[-1])
     assert filepath, "File location not found."
     return Path(filepath.group(1)).name
@@ -26,7 +26,9 @@ class TenantMigrationAPI:
         self.session = session
 
     @overload
-    def export(self, *, desc: str, name: str, subdomain: str, org_name: str, wan_edge_forecast: Optional[int]) -> Task:
+    def export_tenant(
+        self, *, desc: str, name: str, subdomain: str, org_name: str, wan_edge_forecast: Optional[int]
+    ) -> Task:
         """Exports the single-tenant deployment and configuration data from a Cisco vManage instance.
 
         Args:
@@ -34,14 +36,14 @@ class TenantMigrationAPI:
             name (str): Unique name for the tenant in the multitenant deployment.
             subdomain (str): Fully qualified sub-domain name of the tenant.
             org_name (str): Name of the tenant organization. The organization name is case-sensitive.
-            wan_edge_forecast (int): Forecasted number of WAN Edges for given tenant
+            wan_edge_forecast (int): Forecasted number of WAN Edges for given tenant.
         Returns:
             Task: object representing initiated export process
         """
         ...
 
     @overload
-    def export(self, *, tenant: Tenant) -> Task:
+    def export_tenant(self, *, tenant: Tenant) -> Task:
         """Exports the single-tenant deployment and configuration data from a Cisco vManage instance.
 
         Args:
@@ -52,7 +54,7 @@ class TenantMigrationAPI:
         """
         ...
 
-    def export(self, **kwargs) -> Task:
+    def export_tenant(self, **kwargs) -> Task:
         tenant = kwargs.get("tenant", None)
         if tenant is None:
             tenant = Tenant.parse_obj(**kwargs)
@@ -61,18 +63,29 @@ class TenantMigrationAPI:
         process_id = self.session.primitives.tenant_migration.export_tenant_data(tenant).process_id
         return Task(self.session, process_id)
 
-    def download(self, download_path: Path, remote_filename: Optional[str]):
+    def download(self, download_path: Path, remote_filename: str = "default.tar.gz"):
         """Download exported single-tenant deployment and configuration data from a Cisco vManage instance
         to a local file system.
 
         Args:
             download_path (Path): full download path containing a filename eg.: Path("/home/user/tenant-export.tar.gz")
+            remote_filename (str): path to exported tenant migration file on vManage
         """
         tenant_data = self.session.primitives.tenant_migration.download_tenant_data(remote_filename)
         open(download_path, "wb").write(tenant_data)
 
     @overload
-    def collect(self, download_path: Path, timeout: int = 300, *, desc: str, name: str, subdomain: str, org_name: str):
+    def collect(
+        self,
+        download_path: Path,
+        timeout: int = 300,
+        *,
+        desc: str,
+        name: str,
+        subdomain: str,
+        org_name: str,
+        wan_edge_forecast: Optional[int],
+    ):
         """Exports the single-tenant deployment and configuration data from a Cisco vManage instance then
         waits for export to complete and downloads tenant data to a local file system.
 
@@ -83,6 +96,7 @@ class TenantMigrationAPI:
             name (str): Unique name for the tenant in the multitenant deployment.
             subdomain (str): Fully qualified sub-domain name of the tenant.
             org_name (str): Name of the tenant organization. The organization name is case-sensitive.
+            wan_edge_forecast (int): Forecasted number of WAN Edges for given tenant.
         """
         ...
 
@@ -99,7 +113,19 @@ class TenantMigrationAPI:
         ...
 
     def collect(self, download_path: Path, timeout: int = 300, **kwargs):
-        export_task = self.export(**kwargs)
+        export_task = self.export_tenant(**kwargs)
         export_result = export_task.wait_for_completed(timeout_seconds=timeout)
-        remote_filename = get_file_name_from_export_task_result(export_result)
+        remote_filename = _get_file_name_from_export_task_result(export_result)
         return self.download(download_path, remote_filename)
+
+    def import_tenant(self, path: Path) -> Task:
+        """Imports the single-tenant deployment and configuration data into multi-tenant vManage instance.
+
+        Args:
+            path (Path): full path to previously exported single-tenant data file
+
+        Returns:
+            Task: object representing initiated import process
+        """
+        process_id = self.session.primitives.tenant_migration.import_tenant_data(open(path, "rb")).process_id
+        return Task(self.session, process_id)
--- vmngclient/primitives/tenant_migration.py
+++ vmngclient/primitives/tenant_migration.py
@@ -1,4 +1,6 @@
-from typing import Optional
+from pathlib import Path
+from typing import BinaryIO
+from urllib.parse import parse_qsl, urlsplit
 
 from pydantic import BaseModel, Field
 
@@ -6,30 +8,51 @@ from vmngclient.model.tenant import Tenant
 from vmngclient.primitives import APIPrimitiveBase
 
 
-class ExportProcessId(BaseModel):
+class MigrationTokenQueryParams(BaseModel):
+    migration_id: str = Field(alias="migrationId")
+
+
+class ExportInfo(BaseModel):
+    process_id: str = Field(alias="processId")
+
+
+class ImportInfo(BaseModel):
+    process_id: str = Field(alias="processId")
+    migration_token_url: str = Field(alias="migrationTokenURL")
+
+    @property
+    def migration_token_query(self) -> str:
+        return urlsplit(self.migration_token_url).query
+
+    @property
+    def migration_token_query_params(self) -> MigrationTokenQueryParams:
+        query = self.migration_token_query
+        return MigrationTokenQueryParams.parse_obj(parse_qsl(query))
+
+
+class MigrationInfo(BaseModel):
     process_id: str = Field(alias="processId")
 
 
 class TenantMigrationPrimitives(APIPrimitiveBase):
-    def download_tenant_data(self, path: Optional[str] = "default.tar.gz") -> bytes:
+    def download_tenant_data(self, path: str = "default.tar.gz") -> bytes:
         return self.get(f"/tenantmigration/download/{path}").content
 
-    def export_tenant_data(self, tenant: Tenant):
+    def export_tenant_data(self, tenant: Tenant) -> ExportInfo:
         response = self.post("/tenantmigration/export", payload=tenant)
-        return response.dataobj(ExportProcessId, None)
+        return response.dataobj(ExportInfo, None)
 
-    def get_migration_token(self):
-        # GET /tenantmigration/migrationToken
-        pass
+    def get_migration_token(self, params: MigrationTokenQueryParams) -> str:
+        return self.get("/tenantmigration/migrationToken", params=params.dict(by_alias=True)).text
 
-    def import_tenant_data(self):
-        # POST /tenantmigration/import
-        pass
+    def import_tenant_data(self, data: BinaryIO) -> ImportInfo:
+        response = self.post("/tenantmigration/import", files={"file": (Path(data.name).name, data)})
+        return response.dataobj(ImportInfo, None)
 
-    def migrate_network(self):
-        # POST /tenantmigration/networkMigration
-        pass
+    def migrate_network(self, migration_token: str) -> MigrationInfo:
+        response = self.post("/tenantmigration/networkMigration", data=migration_token)
+        return response.dataobj(MigrationInfo, None)
 
     def retrigger_network_migration(self):
         # GET /tenantmigration/networkMigration
-        pass
+        ...
