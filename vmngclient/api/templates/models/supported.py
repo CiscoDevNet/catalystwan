1e5531e39c61ba627af43e8903e0bb8b0fd9c365
tomaszwarda <tomeqqq99@gmail.com>
2023-05-22 14:23:36 +0200

    supported models after main merge

--- vmngclient/api/templates/models/supported.py
+++ vmngclient/api/templates/models/supported.py
@@ -1,11 +1,16 @@
 from enum import Enum
 
 from vmngclient.api.templates.models.cisco_aaa_model import CiscoAAAModel
-from vmngclient.dataclasses import AlarmData
+from vmngclient.api.templates.models.omp_vsmart_model import OMPvSmart
+from vmngclient.api.templates.models.security_vsmart_model import SecurityvSmart
+from vmngclient.api.templates.models.system_vsmart_model import SystemVsmart
 
 
 class FeatureTemplateType(Enum):
     cisco_aaa: str = "cedge_aaa"
+    omp_vsmart: str = "omp-vsmart"
+    security_vsmart: str = "security-vsmart"
+    system_vsmart: str = "system-vsmart"
 
 
-supported_models = [CiscoAAAModel, AlarmData]  # AlarmData provided just for mypy (>1 class needed, only 1 model)
+supported_models = [CiscoAAAModel, OMPvSmart, SecurityvSmart, SystemVsmart]
