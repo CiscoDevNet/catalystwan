3c2c6a2a68137e891534cc5341f81dd9268b11bf
Tomasz Warda <85410520+tomaszwarda@users.noreply.github.com>
2023-05-23 12:17:10 +0200

    AlarmData pretty print and additional fields (#274)
    * alarms str and additional fields
    
    * print date time
    
    * dict instead of ifs
    
    * mypy fix
    
    * optional severity
    
    * non-optional severity
    
    * frozen
    
    * colors change

---
+++ vmngclient/utils/colors.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class PrintColors(Enum):
+    RED_BACKGROUND: str = "\033[41m"
+    RED: str = "\033[31m"
+    YELLOW: str = "\033[33m"
+    BLUE: str = "\033[34m"
+    GREEN: str = "\033[32m"
+    NONE: str = "\033[0m"
