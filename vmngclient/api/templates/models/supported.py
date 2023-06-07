3dccfaeaece070397c1cee80c5907f2eff09c6a8
tehAgitto <kamil.gorski13@gmail.com>
2023-06-07 15:30:00 +0200

    fix supported models

--- vmngclient/api/templates/models/supported.py
+++ vmngclient/api/templates/models/supported.py
@@ -1,16 +1,11 @@
-from enum import Enum
-
 from vmngclient.api.templates.models.cisco_aaa_model import CiscoAAAModel
 from vmngclient.api.templates.models.omp_vsmart_model import OMPvSmart
 from vmngclient.api.templates.models.security_vsmart_model import SecurityvSmart
 from vmngclient.api.templates.models.system_vsmart_model import SystemVsmart
 
-
-class FeatureTemplateType(Enum):
-    cisco_aaa: str = "cedge_aaa"
-    omp_vsmart: str = "omp-vsmart"
-    security_vsmart: str = "security-vsmart"
-    system_vsmart: str = "system-vsmart"
-
-
-supported_models = [CiscoAAAModel, OMPvSmart, SecurityvSmart, SystemVsmart]
+available_models = {
+    "cisco_aaa": CiscoAAAModel,
+    "omp_vsmart": OMPvSmart,
+    "security_vsmart": SecurityvSmart,
+    "system_vsmart": SystemVsmart,
+}
--- vmngclient/utils/feature_template.py
+++ vmngclient/utils/feature_template.py
@@ -1,10 +1,10 @@
-from typing import Callable, Dict, Optional, Union
+from typing import Any, Dict, Optional, Union
 
-from vmngclient.api.templates.models.supported import FeatureTemplateType, supported_models
+from vmngclient.api.templates.models.supported import available_models
 from vmngclient.exceptions import TemplateTypeError
 
 
-def choose_model(type_value: Union[str, FeatureTemplateType]) -> Callable:
+def choose_model(type_value: str) -> Any:
     """Chooses correct model based on provided type
 
     With provided type of feature template searches supported by vmngclient models
@@ -19,12 +19,13 @@ def choose_model(type_value: Union[str, FeatureTemplateType]) -> Callable:
     Raises:
             TemplateTypeError: Raises when the model is not supported by vmngclient.
     """
-    if isinstance(type_value, FeatureTemplateType):
-        type_value = type_value.value
-    for model in supported_models:
-        if getattr(model, "type", None) == type_value:
-            return model
-    raise TemplateTypeError(f"Feature template type '{type_value}' is not supported.")
+    if type_value not in available_models:
+        for model in available_models.values():
+            if model.type == type_value:  # type: ignore
+                return model
+        raise TemplateTypeError(f"Feature template type '{type_value}' is not supported.")
+
+    return available_models[type_value]
 
 
 def find_template_values(
