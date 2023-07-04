eb806b7357d3bcee81a739b7b083d41a66bc5639
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2023-07-04 14:18:34 +0200

    [Core] Logout Session (#304)
    * Delete useless file
    
    * Add logout
    
    * Change the order
    
    * Override close()

--- vmngclient/session.py
+++ vmngclient/session.py
@@ -79,8 +79,7 @@ def create_vManageSession(
         logger: logger for logging API requests
 
     Returns:
-        Session object
-
+        vManageSession: Configured Session to perform tasks on vManage.
     """
     session = vManageSession(url=url, username=username, password=password, port=port, subdomain=subdomain)
     session.auth = vManageAuth(session.base_url, username, password, verify=False)
@@ -131,6 +130,7 @@ def create_vManageSession(
     session.logger.info(
         f"Logged to vManage({session.platform_version}) as {username}. The session type is {session.session_type}"
     )
+
     return session
 
 
@@ -343,6 +343,27 @@ class vManageSession(vManageResponseAdapter, APIPrimitiveClient):
         url_path = f"/dataservice/tenant/{tenant_id}/vsessionid"
         response = self.post(url_path)
         return response.json()["VSessionId"]
+
+    def logout(self) -> vManageResponse:
+        if self.api_version >= Version("20.12"):
+            return self.post("/logout")
+        else:
+            return self.get("/logout")
+
+    def close(self) -> None:
+        """Closes the vManageSession.
+
+        This method is overrided from requests.Session.
+        Firstly it cleans up any resources associated with vManage.
+        Then it closes all adapters and as such the session.
+
+        Note: It is generally recommended to use the session as a context manager
+        using the `with` statement, which ensures that the session is properly
+        closed and resources are cleaned up even in case of exceptions.
+        """
+        self.enable_relogin = False
+        self.logout()
+        super().close()
 
     def __prepare_session(self, verify: bool, auth: Optional[AuthBase]) -> None:
         self.auth = auth
