566c305cf6f849e617ac286ddc322709e4272e60
tehAgitto <kamil.gorski13@gmail.com>
2023-07-19 12:46:58 +0200

    fix mypy

--- vmngclient/api/templates/models/cisco_system.py
+++ vmngclient/api/templates/models/cisco_system.py
@@ -2,23 +2,16 @@
 # from pathlib import Path
 # from typing import ClassVar, List, Optional
 
-# from pydantic import BaseModel, Field
+from pathlib import Path
+from typing import ClassVar
 
-# from vmngclient.api.templates.feature_template import FeatureTemplate
-
-# class CiscoSystemModel(FeatureTemplate):
-#     class Config:
-#         arbitrary_types_allowed = True
-#         allow_population_by_field_name = True
-
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
-#     affinity_group_number: bool = Field(alias="admin-tech-on-failure")
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
-#     admin_tech_on_failure: bool = Field(alias="admin-tech-on-failure")
+from vmngclient.api.templates.feature_template import FeatureTemplate
 
 
-#     payload_path: ClassVar[Path] = Path(__file__).parent / "DEPRECATED"
-#     type: ClassVar[str] = "cisco_system"
+class CiscoSystemModel(FeatureTemplate):
+    class Config:
+        arbitrary_types_allowed = True
+        allow_population_by_field_name = True
+
+    payload_path: ClassVar[Path] = Path(__file__).parent / "DEPRECATED"
+    type: ClassVar[str] = "cisco_system"
--- vmngclient/tests/templates/models/cisco_aaa.py
+++ vmngclient/tests/templates/models/cisco_aaa.py
@@ -1,4 +1,3 @@
-# type: ignore
 from vmngclient.api.templates.models.cisco_aaa_model import (
     CiscoAAAModel,
     DomainStripping,
@@ -6,37 +5,38 @@ from vmngclient.api.templates.models.cisco_aaa_model import (
     RadiusServer,
     User,
 )
+from vmngclient.utils.device_model import DeviceModel
 
 users = [
     User(name="admin", password="str", secret="zyx", privilege="15"),
     User(name="user", password="rnd", secret="dnr", privilege="14"),
 ]
 
-
+# CiscoAAAModel(domain-stripping="?")
 cisco_aaa = CiscoAAAModel(
     name="iuo",
     description="zyx",
-    device_models=["vedge-C8000V"],
+    device_models=[DeviceModel.VEDGE_C8000V],
     user=users,
     authentication_group=True,
     accounting_group=False,
     radius=[
-        RadiusGroup(
+        RadiusGroup(  # type: ignore
             group_name="xyz", vpn=1, source_interface="GIG11", server=[RadiusServer(address="1.1.1.1", key="21")]
         )
     ],
-    domain_stripping=DomainStripping.NO,
+    domain_stripping=DomainStripping.NO,  # type: ignore
 )
 
 cisco_aaa_device_specific = CiscoAAAModel(
     name="cisco_aaa_device_specific",
     description="caaadp",
-    device_models=["vedge-C8000V"],
+    device_models=[DeviceModel.VEDGE_C8000V],
     user=users,
     authentication_group=True,
     accounting_group=False,
     radius=[
-        RadiusGroup(
+        RadiusGroup(  # type: ignore
             group_name="xyz", vpn=1, source_interface="GIG11", server=[RadiusServer(address="1.1.1.1", key="21")]
         )
     ],
