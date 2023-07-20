a40e3e5971fa67f3b65c7745b64bbf8beb5bb136
sbasan <sbasan@cisco.com>
2023-07-20 16:55:03 +0200

    fix bug in decorator, cleanup dependencies

--- vmngclient/utils/pydantic.py
+++ vmngclient/utils/pydantic.py
@@ -1,22 +1,29 @@
+"""
+Module which updates pydantic BaseModel and Field
+Modified for endpoint payload usage to reduce boilerplate in model definitions
+"""
 from functools import wraps
+from typing import Any
 
 from pydantic import BaseModel as _BaseModel
 from pydantic import ConfigDict
 from pydantic import Field as _Field
+from pydantic_core import PydanticUndefined
 
 
 def common_field(func):
+    """
+    Decorator for `pydantic.Field` function changes how `alias` argument works
+    alias is used only in serialization and validation and does not modify construcrtor signature
+    """
+
     @wraps(func)
-    def wrapper(**kwargs):
-        """
-        Wrapper for `pydantic.Field` changes how `alias` argument works
-        alias is used only in serialization and validation and does not modify construcrtor signature
-        """
+    def wrapper(default: Any = PydanticUndefined, **kwargs):
         _kwargs = dict(kwargs)
-        if alias := _kwargs.pop("alias", None) is not None:
+        if (alias := _kwargs.pop("alias", None)) is not None:
             _kwargs["serialization_alias"] = alias
             _kwargs["validation_alias"] = alias
-        return func(_kwargs)
+        return func(default, **_kwargs)
 
     return wrapper
 
@@ -25,4 +32,5 @@ Field = common_field(_Field)
 
 
 class BaseModel(_BaseModel):
+    __doc__ = _BaseModel.__doc__
     model_config = ConfigDict(populate_by_name=True)
