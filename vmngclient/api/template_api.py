35a8e7426cc3a4d216b0b4afafe103d0357c3941
tehAgitto <kamil.gorski13@gmail.com>
2023-08-10 16:45:32 +0200

    rnd

--- vmngclient/api/template_api.py
+++ vmngclient/api/template_api.py
@@ -20,8 +20,8 @@ from vmngclient.api.templates.feature_template_field import FeatureTemplateField
 from vmngclient.api.templates.feature_template_payload import FeatureTemplatePayload
 from vmngclient.api.templates.models.cisco_aaa_model import CiscoAAAModel
 from vmngclient.api.templates.models.cisco_ntp_model import CiscoNTPModel
-from vmngclient.api.templates.models.cisco_system_model import CiscoSystemModel
 from vmngclient.api.templates.models.cisco_snmp_model import CiscoSNMPModel
+from vmngclient.api.templates.models.cisco_system import CiscoSystemModel
 from vmngclient.api.templates.models.cisco_vpn_model import CiscoVPNModel
 from vmngclient.api.templates.models.omp_vsmart_model import OMPvSmart
 from vmngclient.api.templates.models.security_vsmart_model import SecurityvSmart
--- vmngclient/tests/templates/models/__init__.py
+++ vmngclient/tests/templates/models/__init__.py
@@ -6,5 +6,6 @@ from vmngclient.tests.templates.models.cisco_aaa import cisco_aaa
 # from vmngclient.tests.templates.models.omp_vsmart import default_omp, omp_2, omp_3
 from vmngclient.tests.templates.models.cisco_system import default_cisco_system
 
-__all__ = list(set(locals()) - not_my_data)
-print(locals()['cisco_system'])
+__all__ = ["cisco_aaa", "default_cisco_system"]
+
+
