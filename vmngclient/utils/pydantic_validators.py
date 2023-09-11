6c134a7758bb968ca47565ce0030d728650fc23d
Mateusz Slugocki <mslugock@cisco.com>
2023-09-11 04:51:05 -0700

    added cisco_sig model

--- vmngclient/utils/pydantic_validators.py
+++ vmngclient/utils/pydantic_validators.py
@@ -1,3 +1,5 @@
+import ipaddress
+
 from pydantic import BaseModel, root_validator
 
 
@@ -6,5 +8,14 @@ class ConvertBoolToStringModel(BaseModel):
     def convert_bool_to_string_validator(cls, values):
         for key, value in values.items():
             if isinstance(value, bool):
+                values[key] = str(value).lower()
+        return values
+
+
+class ConvertIPToStringModel(BaseModel):
+    @root_validator  # type: ignore
+    def convert_ip_to_string_validator(cls, values):
+        for key, value in values.items():
+            if isinstance(value, ipaddress._BaseAddress):
                 values[key] = str(value).lower()
         return values
