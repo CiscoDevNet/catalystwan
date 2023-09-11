f1a3deaca794539446c493bf4582a568e17fa057
Mateusz Slugocki <mslugock@cisco.com>
2023-09-11 05:01:10 -0700

    Added ip to string converter

--- vmngclient/utils/pydantic_validators.py
+++ vmngclient/utils/pydantic_validators.py
@@ -1,4 +1,5 @@
 import ipaddress
+from typing import Any
 
 from pydantic import BaseModel, root_validator
 
@@ -16,6 +17,14 @@ class ConvertIPToStringModel(BaseModel):
     @root_validator  # type: ignore
     def convert_ip_to_string_validator(cls, values):
         for key, value in values.items():
-            if isinstance(value, ipaddress._BaseAddress):
-                values[key] = str(value).lower()
+            values[key] = convert_ip_to_string(value)
         return values
+
+
+def convert_ip_to_string(values: Any):
+    if isinstance(values, list):
+        for index, ip in enumerate(values):
+            values[index] = convert_ip_to_string(ip)
+    if isinstance(values, ipaddress._BaseAddress):
+        values = str(values)
+    return values
