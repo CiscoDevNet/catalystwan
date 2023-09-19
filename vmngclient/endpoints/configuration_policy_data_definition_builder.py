afa1d492adfc65b757301a7526bf76571d242acf
sbasan <sbasan@cisco.com>
2023-09-19 11:14:40 +0200

    refactor: move policy related models to separate directory

--- vmngclient/endpoints/configuration_policy_data_definition_builder.py
+++ vmngclient/endpoints/configuration_policy_data_definition_builder.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, Field, IPvAnyNetwork
 from typing_extensions import Annotated, Literal
 
 from vmngclient.endpoints import APIEndpoints, delete, get, post, put
-from vmngclient.model.policy_definition import (
+from vmngclient.model.policy.policy_definition import (
     PolicyDefinition,
     PolicyDefinitionCreationPayload,
     PolicyDefinitionEditPayload,
