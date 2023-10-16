b39c5bb303e08f412b051607f4bd77df121e3555
Szymon Basan <116343782+sbasan@users.noreply.github.com>
2023-10-16 13:24:52 +0200

    add: missing settings, endpoints for edit, SingleTenantView (#385)
    * add: missing settings, endpoints for edit
    
    * add: new session type for single-tenant mode
    
    * annotate selected settings with allowed views
    
    * views for cloudservices

--- vmngclient/utils/session_type.py
+++ vmngclient/utils/session_type.py
@@ -2,6 +2,7 @@ from enum import Enum, auto
 
 
 class SessionType(Enum):
+    SINGLE_TENANT = auto()
     PROVIDER = auto()
     TENANT = auto()
     PROVIDER_AS_TENANT = auto()
@@ -11,3 +12,4 @@ class SessionType(Enum):
 ProviderView = SessionType.PROVIDER
 TenantView = SessionType.TENANT
 ProviderAsTenantView = SessionType.PROVIDER_AS_TENANT
+SingleTenantView = SessionType.SINGLE_TENANT
