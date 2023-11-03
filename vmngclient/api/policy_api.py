f26e017f3e16ed4f2c52939cf95f0c2485529616
sbasan <sbasan@cisco.com>
2023-11-03 06:31:50 +0100

    refactor: PolicyAPI

--- vmngclient/api/policy_api.py
+++ vmngclient/api/policy_api.py
@@ -174,7 +174,7 @@ POLICY_DEFINITION_ENDPOINTS_MAP: Mapping[type, type] = {
 SupportedPolicyDefinitions = Union[RuleSet, SecurityGroup, ZoneBasedFWPolicy, DataPolicy]
 
 
-class PolicyAPI:
+class PolicyListsAPI:
     def __init__(self, session: vManageSession):
         self.session = session
 
@@ -184,304 +184,316 @@ class PolicyAPI:
             raise TypeError(f"Unsupported policy list type: {payload_type}")
         return builder_class(self.session)
 
+    def create(self, policy_list: AllPolicyLists) -> str:
+        builder = self.__get_list_endpoints_instance(type(policy_list))
+        return builder.create_policy_list(payload=policy_list).list_id
+
+    def edit(self, id: str, policy_list: AllPolicyLists) -> None:
+        builder = self.__get_list_endpoints_instance(type(policy_list))
+        builder.edit_policy_list(id=id, payload=policy_list)
+
+    def delete(self, type: Type[AllPolicyLists], id: str) -> None:
+        builder = self.__get_list_endpoints_instance(type)
+        builder.delete_policy_list(id=id)
+
+    @overload
+    def get(self, type: Type[AppList]) -> DataSequence[AppListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[AppProbeClassList]) -> DataSequence[AppProbeClassListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ASPathList]) -> DataSequence[ASPathListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ClassMapList]) -> DataSequence[ClassMapListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ColorList]) -> DataSequence[ColorListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[CommunityList]) -> DataSequence[CommunityListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[DataIPv6PrefixList]) -> DataSequence[DataIPv6PrefixListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[DataPrefixList]) -> DataSequence[DataPrefixListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ExpandedCommunityList]) -> DataSequence[ExpandedCommunityListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[FQDNList]) -> DataSequence[FQDNListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[GeoLocationList]) -> DataSequence[GeoLocationListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[IPSSignatureList]) -> DataSequence[IPSSignatureListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[IPv6PrefixList]) -> DataSequence[IPv6PrefixListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[LocalAppList]) -> DataSequence[LocalAppListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[LocalDomainList]) -> DataSequence[LocalDomainListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[MirrorList]) -> DataSequence[MirrorListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[PolicerList]) -> DataSequence[PolicerListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[PortList]) -> DataSequence[PortListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[PreferredColorGroupList]) -> DataSequence[PreferredColorGroupListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[PrefixList]) -> DataSequence[PrefixListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ProtocolNameList]) -> DataSequence[ProtocolNameListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[SiteList]) -> DataSequence[SiteListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[SLAClassList]) -> DataSequence[SLAClassListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[TLOCList]) -> DataSequence[TLOCListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[URLBlackList]) -> DataSequence[URLBlackListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[URLWhiteList]) -> DataSequence[URLWhiteListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[VPNList]) -> DataSequence[VPNListInfo]:
+        ...
+
+    @overload
+    def get(self, type: Type[ZoneList]) -> DataSequence[ZoneListInfo]:
+        ...
+
+    def get(self, type: Type[AllPolicyLists]) -> Sequence[PolicyListInfo]:
+        builder = self.__get_list_endpoints_instance(type)
+        return builder.get_policy_lists()
+
+    @overload
+    def get_by_id(self, type: Type[AppList], id: str) -> AppListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[AppProbeClassList], id: str) -> AppProbeClassListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ASPathList], id: str) -> ASPathListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ClassMapList], id: str) -> ClassMapListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ColorList], id: str) -> ColorListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[CommunityList], id: str) -> CommunityListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[DataIPv6PrefixList], id: str) -> DataIPv6PrefixListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[DataPrefixList], id: str) -> DataPrefixListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ExpandedCommunityList], id: str) -> ExpandedCommunityListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[FQDNList], id: str) -> FQDNListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[GeoLocationList], id: str) -> GeoLocationListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[IPSSignatureList], id: str) -> IPSSignatureListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[IPv6PrefixList], id: str) -> IPv6PrefixListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[LocalAppList], id: str) -> LocalAppListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[LocalDomainList], id: str) -> LocalDomainListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[MirrorList], id: str) -> MirrorListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[PolicerList], id: str) -> PolicerListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[PortList], id: str) -> PortListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[PreferredColorGroupList], id: str) -> PreferredColorGroupListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[PrefixList], id: str) -> PrefixListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ProtocolNameList], id: str) -> ProtocolNameListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[SiteList], id: str) -> SiteListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[SLAClassList], id: str) -> SLAClassListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[TLOCList], id: str) -> TLOCListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[URLBlackList], id: str) -> URLBlackListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[URLWhiteList], id: str) -> URLWhiteListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[VPNList], id: str) -> VPNListInfo:
+        ...
+
+    @overload
+    def get_by_id(self, type: Type[ZoneList], id: str) -> ZoneListInfo:
+        ...
+
+    def get_by_id(self, type: Type[AllPolicyLists], id: str) -> PolicyListInfo:
+        builder = self.__get_list_endpoints_instance(type)
+        return builder.get_lists_by_id(id=id)
+
+
+class PolicyDefinitionsAPI:
+    def __init__(self, session: vManageSession):
+        self.session = session
+
     def __get_definition_endpoints_instance(self, payload_type: type) -> PolicyDefinitionEndpoints:
         builder_class = POLICY_DEFINITION_ENDPOINTS_MAP.get(payload_type)
         if builder_class is None:
             raise TypeError(f"Unsupported policy definition type: {payload_type}")
         return builder_class(self.session)
 
-    def create_list(self, policy_list: AllPolicyLists) -> str:
-        builder = self.__get_list_endpoints_instance(type(policy_list))
-        return builder.create_policy_list(payload=policy_list).list_id
-
-    def edit_list(self, id: str, policy_list: AllPolicyLists) -> None:
-        builder = self.__get_list_endpoints_instance(type(policy_list))
-        builder.edit_policy_list(id=id, payload=policy_list)
-
-    def delete_list(self, type: Type[AllPolicyLists], id: str) -> None:
-        builder = self.__get_list_endpoints_instance(type)
-        builder.delete_policy_list(id=id)
-
-    def create_definition(self, policy_definition: SupportedPolicyDefinitions) -> str:
+    def create(self, policy_definition: SupportedPolicyDefinitions) -> str:
         builder = self.__get_definition_endpoints_instance(type(policy_definition))
         return builder.create_policy_definition(payload=policy_definition).definition_id
 
-    def edit_definition(self, id: str, policy_definition: SupportedPolicyDefinitions) -> PolicyDefinitionEditResponse:
+    def edit(self, id: str, policy_definition: SupportedPolicyDefinitions) -> PolicyDefinitionEditResponse:
         builder = self.__get_definition_endpoints_instance(type(policy_definition))
         return builder.edit_policy_definition(id=id, payload=policy_definition)
 
-    def delete_definition(self, type: Type[SupportedPolicyDefinitions], id: str) -> None:
+    def delete(self, type: Type[SupportedPolicyDefinitions], id: str) -> None:
         builder = self.__get_definition_endpoints_instance(type)
         builder.delete_policy_definition(id=id)
 
     @overload
-    def get_lists(self, type: Type[AppList]) -> DataSequence[AppListInfo]:
+    def get(self, type: Type[DataPolicy]) -> DataSequence[PolicyDefinitionInfo]:
         ...
 
     @overload
-    def get_lists(self, type: Type[AppProbeClassList]) -> DataSequence[AppProbeClassListInfo]:
+    def get(self, type: Type[RuleSet]) -> DataSequence[RuleSetInfo]:
         ...
 
     @overload
-    def get_lists(self, type: Type[ASPathList]) -> DataSequence[ASPathListInfo]:
+    def get(self, type: Type[SecurityGroup]) -> DataSequence[SecurityGroupInfo]:
         ...
 
     @overload
-    def get_lists(self, type: Type[ClassMapList]) -> DataSequence[ClassMapListInfo]:
+    def get(self, type: Type[ZoneBasedFWPolicy]) -> DataSequence[ZoneBasedFWPolicyInfo]:
         ...
 
-    @overload
-    def get_lists(self, type: Type[ColorList]) -> DataSequence[ColorListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[CommunityList]) -> DataSequence[CommunityListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[DataIPv6PrefixList]) -> DataSequence[DataIPv6PrefixListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[DataPrefixList]) -> DataSequence[DataPrefixListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[ExpandedCommunityList]) -> DataSequence[ExpandedCommunityListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[FQDNList]) -> DataSequence[FQDNListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[GeoLocationList]) -> DataSequence[GeoLocationListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[IPSSignatureList]) -> DataSequence[IPSSignatureListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[IPv6PrefixList]) -> DataSequence[IPv6PrefixListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[LocalAppList]) -> DataSequence[LocalAppListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[LocalDomainList]) -> DataSequence[LocalDomainListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[MirrorList]) -> DataSequence[MirrorListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[PolicerList]) -> DataSequence[PolicerListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[PortList]) -> DataSequence[PortListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[PreferredColorGroupList]) -> DataSequence[PreferredColorGroupListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[PrefixList]) -> DataSequence[PrefixListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[ProtocolNameList]) -> DataSequence[ProtocolNameListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[SiteList]) -> DataSequence[SiteListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[SLAClassList]) -> DataSequence[SLAClassListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[TLOCList]) -> DataSequence[TLOCListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[URLBlackList]) -> DataSequence[URLBlackListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[URLWhiteList]) -> DataSequence[URLWhiteListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[VPNList]) -> DataSequence[VPNListInfo]:
-        ...
-
-    @overload
-    def get_lists(self, type: Type[ZoneList]) -> DataSequence[ZoneListInfo]:
-        ...
-
-    def get_lists(self, type: Type[AllPolicyLists]) -> Sequence[PolicyListInfo]:
-        builder = self.__get_list_endpoints_instance(type)
-        return builder.get_policy_lists()
-
-    @overload
-    def get_list(self, type: Type[AppList], id: str) -> AppListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[AppProbeClassList], id: str) -> AppProbeClassListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ASPathList], id: str) -> ASPathListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ClassMapList], id: str) -> ClassMapListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ColorList], id: str) -> ColorListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[CommunityList], id: str) -> CommunityListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[DataIPv6PrefixList], id: str) -> DataIPv6PrefixListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[DataPrefixList], id: str) -> DataPrefixListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ExpandedCommunityList], id: str) -> ExpandedCommunityListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[FQDNList], id: str) -> FQDNListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[GeoLocationList], id: str) -> GeoLocationListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[IPSSignatureList], id: str) -> IPSSignatureListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[IPv6PrefixList], id: str) -> IPv6PrefixListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[LocalAppList], id: str) -> LocalAppListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[LocalDomainList], id: str) -> LocalDomainListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[MirrorList], id: str) -> MirrorListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[PolicerList], id: str) -> PolicerListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[PortList], id: str) -> PortListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[PreferredColorGroupList], id: str) -> PreferredColorGroupListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[PrefixList], id: str) -> PrefixListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ProtocolNameList], id: str) -> ProtocolNameListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[SiteList], id: str) -> SiteListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[SLAClassList], id: str) -> SLAClassListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[TLOCList], id: str) -> TLOCListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[URLBlackList], id: str) -> URLBlackListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[URLWhiteList], id: str) -> URLWhiteListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[VPNList], id: str) -> VPNListInfo:
-        ...
-
-    @overload
-    def get_list(self, type: Type[ZoneList], id: str) -> ZoneListInfo:
-        ...
-
-    def get_list(self, type: Type[AllPolicyLists], id: str) -> PolicyListInfo:
-        builder = self.__get_list_endpoints_instance(type)
-        return builder.get_lists_by_id(id=id)
-
-    @overload
-    def get_definitions(self, type: Type[DataPolicy]) -> DataSequence[PolicyDefinitionInfo]:
-        ...
-
-    @overload
-    def get_definitions(self, type: Type[RuleSet]) -> DataSequence[RuleSetInfo]:
-        ...
-
-    @overload
-    def get_definitions(self, type: Type[SecurityGroup]) -> DataSequence[SecurityGroupInfo]:
-        ...
-
-    @overload
-    def get_definitions(self, type: Type[ZoneBasedFWPolicy]) -> DataSequence[ZoneBasedFWPolicyInfo]:
-        ...
-
-    def get_definitions(self, type: Type[SupportedPolicyDefinitions]) -> Sequence[PolicyDefinitionInfo]:
+    def get(self, type: Type[SupportedPolicyDefinitions]) -> Sequence[PolicyDefinitionInfo]:
         builder = self.__get_definition_endpoints_instance(type)
         return builder.get_definitions()
 
     @overload
-    def get_definition(self, type: Type[DataPolicy], id: str) -> DataPolicyGetResponse:
+    def get_by_id(self, type: Type[DataPolicy], id: str) -> DataPolicyGetResponse:
         ...
 
     @overload
-    def get_definition(self, type: Type[RuleSet], id: str) -> RuleSetInfo:
+    def get_by_id(self, type: Type[RuleSet], id: str) -> RuleSetInfo:
         ...
 
     @overload
-    def get_definition(self, type: Type[SecurityGroup], id: str) -> SecurityGroupInfo:
+    def get_by_id(self, type: Type[SecurityGroup], id: str) -> SecurityGroupInfo:
         ...
 
     @overload
-    def get_definition(self, type: Type[ZoneBasedFWPolicy], id: str) -> ZoneBasedFWPolicyGetResponse:
+    def get_by_id(self, type: Type[ZoneBasedFWPolicy], id: str) -> ZoneBasedFWPolicyGetResponse:
         ...
 
-    def get_definition(self, type: Type[SupportedPolicyDefinitions], id: str) -> Any:
+    def get_by_id(self, type: Type[SupportedPolicyDefinitions], id: str) -> Any:
         builder = self.__get_definition_endpoints_instance(type)
         return builder.get_policy_definition(id=id)
+
+
+class PolicyAPI:
+    def __init__(self, session: vManageSession):
+        self.session = session
+        self.definitions = PolicyDefinitionsAPI(session)
+        self.lists = PolicyListsAPI(session)
