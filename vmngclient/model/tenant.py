339e905a7e4ee60930c80025a9461d633427a10b
Szymon Basan <116343782+sbasan@users.noreply.github.com>
2023-11-08 22:05:38 +0100

    add missing description field (#395)
    * add missing description field
    
    * update unittests
    
    * bump release version to 0.17.2

--- vmngclient/model/tenant.py
+++ vmngclient/model/tenant.py
@@ -51,6 +51,7 @@ class Tenant(BaseModel):
 
 class TenantExport(BaseModel):
     name: str
+    desc: str
     org_name: str = Field(alias="orgName")
     subdomain: str = Field(alias="subDomain")
     wan_edge_forecast: Optional[int] = Field(alias="wanEdgeForecast")
