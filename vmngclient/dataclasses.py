593768f5653d088c6e540a46c5a9ffd79795e7a1
sbasan <sbasan@cisco.com>
2023-11-09 11:33:12 +0100

    add missing fields to Device model

--- vmngclient/dataclasses.py
+++ vmngclient/dataclasses.py
@@ -136,6 +136,8 @@ class Device(DataclassBase):
     board_serial: Optional[str] = field(default=None, metadata={FIELD_NAME: "board-serial"})
     vedgeCertificateState: Optional[str] = field(default=None, metadata={FIELD_NAME: "vedgeCertificateState"})  # TODO
     chasis_number: Optional[str] = field(default=None, metadata={FIELD_NAME: "chasisNumber"})
+    site_id: Optional[str] = field(default=None, metadata={FIELD_NAME: "site-id"})
+    site_name: Optional[str] = field(default=None, metadata={FIELD_NAME: "site-name"})
 
     @property
     def is_reachable(self) -> bool:
