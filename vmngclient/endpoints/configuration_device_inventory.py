068ecf7aa983498caaea8cc6dcdf7574ca8b97dd
kagorski <kamil.gorski13@gmail.com>
2023-11-17 16:16:36 +0100

    add endpoints

--- vmngclient/endpoints/configuration_device_inventory.py
+++ vmngclient/endpoints/configuration_device_inventory.py
@@ -1,9 +1,11 @@
 # mypy: disable-error-code="empty-body"
-from typing import List
+from enum import Enum
+from typing import List, Optional
 
 from pydantic.v1 import BaseModel, Field
 
-from vmngclient.endpoints import APIEndpoints, post, versions
+from vmngclient.endpoints import APIEndpoints, delete, post, versions
+from vmngclient.utils.personality import Personality
 
 
 class UnlockDeviceDetail(BaseModel):
@@ -20,8 +22,42 @@ class DeviceUnlockResponse(BaseModel):
     parentTaskId: str
 
 
+class Protocol(str, Enum):
+    DTLS = "DTLS"
+    TLS = "TLS"
+
+
+class DeviceCreationPayload(BaseModel):
+    device_ip: str = Field(alias="deviceIP")
+    generate_csr: bool = Field(alias="generateCSR")
+    password: str
+    personality: Personality
+    port: Optional[str]
+    protocol: Protocol = Protocol.DTLS
+    username: str
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class DeviceDeletionResponse(BaseModel):
+    local_delete_from_db: bool = Field(alias="localDeleteFromDB")
+    id: str
+
+    class Config:
+        allow_population_by_field_name = True
+
+
 class ConfigurationDeviceInventory(APIEndpoints):
     @versions(supported_versions=(">=20.9"), raises=False)
     @post("/system/device/{device_uuid}/unlock")
     def unlock(self, device_uuid: str, payload: DeviceUnlockPayload) -> DeviceUnlockResponse:
+        ...
+
+    @post("/system/device")
+    def create_device(self, payload: DeviceCreationPayload) -> None:
+        ...
+
+    @delete("/system/device/{uuid}")
+    def delete_device(self, uuid: str) -> DeviceDeletionResponse:
         ...
