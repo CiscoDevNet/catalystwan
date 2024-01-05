e3efcae553e977b7e80fb7fda62cbd25a10ee7eb
kagorski <kamil.gorski13@gmail.com>
2024-01-05 15:02:03 +0100

    Add transport

--- vmngclient/endpoints/configuration/common.py
+++ vmngclient/endpoints/configuration/common.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 from enum import Enum
+from typing import Optional
 
 from pydantic import BaseModel, ConfigDict, Field
+
+from vmngclient.model.configuration.feature_profile.common import ProfileType
 
 
 class SchemaType(str, Enum):
@@ -19,3 +23,33 @@ class Solution(str, Enum):
     SDWAN = "sdwan"
     NFVIRTUAL = "nfvirtual"
     SDROUTING = "sd-routing"
+
+
+class FeatureProfileInfo(BaseModel):
+    profile_id: str = Field(alias="profileId")
+    profile_name: str = Field(alias="profileName")
+    solution: str
+    profile_type: ProfileType = Field(alias="profileType")
+    created_by: str = Field(alias="createdBy")
+    last_updated_by: str = Field(alias="lastUpdatedBy")
+    description: str
+    created_on: datetime = Field(alias="createdOn")
+    last_updated_on: datetime = Field(alias="lastUpdatedOn")
+
+
+class FeatureProfileCreationPayload(BaseModel):
+    name: str
+    description: str
+
+
+class FeatureProfileCreationResponse(BaseModel):
+    id: str
+
+
+class ParcelId(BaseModel):
+    id: str = Field(alias="parcelId")
+
+
+class GetFeatureProfilesPayload(BaseModel):
+    limit: Optional[int]
+    offset: Optional[int]
---
+++ vmngclient/endpoints/configuration/feature_profile/sdwan/transport.py
@@ -0,0 +1,46 @@
+# mypy: disable-error-code="empty-body"
+
+from typing import Optional
+
+from vmngclient.api.configuration_groups.parcel import MainParcel
+from vmngclient.endpoints import JSON, APIEndpoints, delete, get, post, versions
+from vmngclient.endpoints.configuration.common import GetFeatureProfilesPayload, ParcelId, SchemaTypeQuery
+from vmngclient.model.configuration.feature_profile.common import (
+    FeatureProfileCreationPayload,
+    FeatureProfileCreationResponse,
+    FeatureProfileInfo,
+)
+from vmngclient.typed_list import DataSequence
+
+
+class TransportFeatureProfile(APIEndpoints):
+    @versions(supported_versions=(">=20.9"), raises=False)
+    @get("/v1/feature-profile/sdwan/transport")
+    def get_sdwan_transport_feature_profiles(
+        self, payload: Optional[GetFeatureProfilesPayload]
+    ) -> DataSequence[FeatureProfileInfo]:
+        ...
+
+    @versions(supported_versions=(">=20.9"), raises=False)
+    @get("/v1/feature-profile/sdwan/transport/cellular-controller/schema", resp_json_key="request")
+    def get_sdwan_transport_cellular_controller_parcel_schema(self, params: SchemaTypeQuery) -> JSON:
+        ...
+
+    @versions(supported_versions=(">=20.9"), raises=False)
+    @post("/v1/feature-profile/sdwan/transport")
+    def create_sdwan_transport_feature_profile(
+        self, payload: FeatureProfileCreationPayload
+    ) -> FeatureProfileCreationResponse:
+        ...
+
+    @versions(supported_versions=(">=20.9"), raises=False)
+    @delete("/v1/feature-profile/sdwan/transport/{transport_id}")
+    def delete_sdwan_transport_feature_profile(self, transport_id: str) -> None:
+        ...
+
+    @versions(supported_versions=(">=20.9"), raises=False)
+    @post("/v1/feature-profile/sdwan/transport/{transport_id}/cellular-controller")
+    def create_cellular_controller_profile_parcel_for_transport(
+        self, transport_id: str, payload: MainParcel
+    ) -> ParcelId:
+        ...
