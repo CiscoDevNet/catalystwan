2b070e800e5b0292f2f0612b032a14b85a1b7d2a
kagorski <kamil.gorski13@gmail.com>
2024-01-18 17:23:12 +0100

    tmp

--- vmngclient/api/configuration_groups/parcel.py
+++ vmngclient/api/configuration_groups/parcel.py
@@ -1,12 +1,37 @@
 from enum import Enum
 from typing import Any, Generic, Optional, TypeVar
 
-from pydantic import BaseModel, ConfigDict, Field, SerializeAsAny
+from pydantic import BaseModel, ConfigDict, Field, SerializeAsAny, model_validator
 
 T = TypeVar("T")
 
 
+class _ParcelBase(BaseModel):
+    name: str
+    description: str
+    data: Optional[Any] = None
+
+    @model_validator(mode="wrap")  # type: ignore
+    def envelope_data(self, handler) -> "_ParcelBase":
+        old_obj = handler(self)
+
+        model_dict = old_obj.model_dump(include={"name", "description"}, exclude_none=True, by_alias=True)
+        model_dict.update(
+            {"data": old_obj.model_dump(exclude={"name", "description"}, exclude_none=True, by_alias=True)}
+        )
+
+        verified_dict = handler(model_dict)
+        for i, pair in enumerate(verified_dict):
+            name, _ = pair
+            if name not in ["name", "description", "data"]:
+                verified_dict.__setattr__(name, None)
+
+        return verified_dict
+
+
 class Parcel(BaseModel):
+    parcel_name: str
+    parcel_description: str
     model_config = ConfigDict(extra="forbid", arbitrary_types_allowed=True, populate_by_name=True)
 
 
@@ -65,3 +90,9 @@ class MainParcel(BaseModel):
     name: str = Field(min_length=1, max_length=128, pattern=r'^[^&<>! "]+$')
     description: Optional[str] = Field(default=None, description="Set the parcel description")
     data: SerializeAsAny[Parcel]
+
+
+class ParcelAttribute:
+    @staticmethod
+    def create_global(value: Any):
+        return Global[type(value)](value=value)  # type: ignore
