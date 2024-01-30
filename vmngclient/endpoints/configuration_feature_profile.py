198540693ed42b3c5048b9160d848364b230e202
Kamil Górski <33100242+tehAgitto@users.noreply.github.com>
2024-01-30 16:30:55 +0100

    Add Implicit Creation of Global Variables in Configuration Groups (#425)
    * add validator
    
    * cleanu
    p
    
    * tmp
    
    * cleanup
    
    * cleanup
    
    * add valid model
    
    * fix import
    
    * fix serializer

--- vmngclient/endpoints/configuration_feature_profile.py
+++ vmngclient/endpoints/configuration_feature_profile.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict, Field
 
-from vmngclient.api.configuration_groups.parcel import MainParcel
+from vmngclient.api.configuration_groups.parcel import _ParcelBase
 from vmngclient.endpoints import JSON, APIEndpoints, delete, get, post, put, versions
 from vmngclient.models.configuration.feature_profile.common import (
     FeatureProfileCreationPayload,
@@ -86,13 +86,13 @@ class ConfigurationFeatureProfile(APIEndpoints):
 
     @versions(supported_versions=(">=20.9"), raises=False)
     @post("/v1/feature-profile/sdwan/system/{system_id}/aaa")
-    def create_aaa_profile_parcel_for_system(self, system_id: str, payload: MainParcel) -> ParcelId:
+    def create_aaa_profile_parcel_for_system(self, system_id: str, payload: _ParcelBase) -> ParcelId:
         ...
 
     @versions(supported_versions=(">=20.9"), raises=False)
     @post("/v1/feature-profile/sdwan/transport/{transport_id}/cellular-controller")
     def create_cellular_controller_profile_parcel_for_transport(
-        self, transport_id: str, payload: MainParcel
+        self, transport_id: str, payload: _ParcelBase
     ) -> ParcelId:
         ...
 
