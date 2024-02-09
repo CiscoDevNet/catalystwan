ea279228c4d82ff1fea52068f095c3ce47aaaa9d
sbasan <sbasan@cisco.com>
2024-02-09 18:26:28 +0100

    draft

---
+++ catalystwan/endpoints/configuration/config_migration.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+from catalystwan.models.policy import (
+    AnyPolicyDefinition,
+    AnyPolicyList,
+    CentralizedPolicy,
+    LocalizedPolicy,
+    SecurityPolicy,
+)
+
+
+class UX1Policies(BaseModel):
+    centralized_policies: List[CentralizedPolicy] = Field(default=[], serialization_alias="centralizedPolicies")
+    localized_policies: List[LocalizedPolicy] = Field(default=[], serialization_alias="localizedPolicies")
+    security_policies: List[SecurityPolicy] = Field(default=[], serialization_alias="securityPolicies")
+    policy_definitions: List[AnyPolicyDefinition] = Field(default=[], serialization_alias="policyDefinitions")
+    policy_lists: List[AnyPolicyList] = Field(default=[], serialization_alias="policyLists")
+
+
+class UX1Templates(BaseModel):
+    pass
+
+
+class UX1Config(BaseModel):
+    # All UX1 Configuration items - Mega Model
+    policies: UX1Policies
+    templates: UX1Templates
+
+
+class UX2Config(BaseModel):
+    # All UX2 Configuration items - Mega Model
+    pass
