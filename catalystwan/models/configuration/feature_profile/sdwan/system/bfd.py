578ab8432930c1ebec7165be7349b402b2c92dea
Jakub Krajewski <jakrajew@cisco.com>
2024-02-26 14:56:57 +0100

    Fix conflicts

--- catalystwan/models/configuration/feature_profile/sdwan/system/bfd.py
+++ catalystwan/models/configuration/feature_profile/sdwan/system/bfd.py
@@ -1,9 +1,42 @@
-from typing import Literal
+from typing import List, Literal, Optional, Union
 
-from pydantic import Field
+from pydantic import AliasPath, BaseModel, ConfigDict, Field
 
-from catalystwan.api.configuration_groups.parcel import _ParcelBase
+from catalystwan.api.configuration_groups.parcel import Global, _ParcelBase, as_global
+from catalystwan.models.common import TLOCColor
+from catalystwan.utils.config_migration.converters.recast import DefaultGlobalBool, DefaultGlobalColorLiteral
+
+DEFAULT_BFD_COLOR_MULTIPLIER = as_global(7)
+DEFAULT_BFD_DSCP = as_global(48)
+DEFAULT_BFD_HELLO_INTERVAL = as_global(1000)
+DEFAULT_BFD_POLL_INTERVAL = as_global(600000)
+DEFAULT_BFD_MULTIPLIER = as_global(6)
+
+
+class Color(BaseModel):
+    color: Union[DefaultGlobalColorLiteral, Global[TLOCColor]]
+    hello_interval: Optional[Global[int]] = Field(
+        default=DEFAULT_BFD_HELLO_INTERVAL, validation_alias="helloInterval", serialization_alias="helloInterval"
+    )
+    multiplier: Optional[Global[int]] = DEFAULT_BFD_COLOR_MULTIPLIER
+    pmtu_discovery: Optional[Union[DefaultGlobalBool, Global[bool]]] = Field(
+        default=as_global(True), validation_alias="pmtuDiscovery", serialization_alias="pmtuDiscovery"
+    )
+    dscp: Optional[Global[int]] = DEFAULT_BFD_DSCP
+    model_config = ConfigDict(populate_by_name=True)
 
 
 class BFDParcel(_ParcelBase):
     type_: Literal["bfd"] = Field(default="bfd", exclude=True)
+    model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True)
+
+    multiplier: Optional[Global[int]] = Field(
+        default=DEFAULT_BFD_MULTIPLIER, validation_alias=AliasPath("data", "multiplier")
+    )
+    poll_interval: Optional[Global[int]] = Field(
+        default=DEFAULT_BFD_POLL_INTERVAL, validation_alias=AliasPath("data", "pollInterval")
+    )
+    default_dscp: Optional[Global[int]] = Field(
+        default=DEFAULT_BFD_DSCP, validation_alias=AliasPath("data", "defaultDscp")
+    )
+    colors: Optional[List[Color]] = Field(default=None, validation_alias=AliasPath("data", "colors"))
