27e6ceeaa925c2dd50ef2f6b7d3f7aaaf5e0bdb6
Jakub Krajewski <jakrajew@cisco.com>
2024-02-28 11:52:33 +0100

    Write tests for API to check correct mapping and request path

---
+++ catalystwan/tests/test_feature_profile_api.py
@@ -0,0 +1,82 @@
+import unittest
+from unittest.mock import patch
+from uuid import UUID
+
+from parameterized import parameterized  # type: ignore
+
+from catalystwan.api.feature_profile_api import SystemFeatureProfileAPI
+from catalystwan.models.configuration.feature_profile.sdwan.system import (
+    AAAParcel,
+    BannerParcel,
+    BasicParcel,
+    BFDParcel,
+    GlobalParcel,
+    LoggingParcel,
+    MRFParcel,
+    NTPParcel,
+    OMPParcel,
+    SecurityParcel,
+    SNMPParcel,
+)
+
+endpoint_mapping = {
+    AAAParcel: "aaa",
+    BannerParcel: "banner",
+    BasicParcel: "basic",
+    BFDParcel: "bfd",
+    GlobalParcel: "global",
+    LoggingParcel: "logging",
+    MRFParcel: "mrf",
+    NTPParcel: "ntp",
+    OMPParcel: "omp",
+    SecurityParcel: "security",
+    SNMPParcel: "snmp",
+}
+
+
+class TestSystemFeatureProfileAPI(unittest.TestCase):
+    def setUp(self):
+        self.profile_uuid = UUID("054d1b82-9fa7-43c6-98fb-4355da0d77ff")
+        self.parcel_uuid = UUID("7113505f-8cec-4420-8799-1a209357ba7e")
+
+    @parameterized.expand(endpoint_mapping.items())
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.endpoints.configuration.feature_profile.sdwan.system.SystemFeatureProfile")
+    def test_delete_method_with_valid_arguments(self, parcel, expected_path, mock_endpoint, mock_session):
+        # Arrange
+        api = SystemFeatureProfileAPI(mock_session)
+        api.endpoint = mock_endpoint
+
+        # Act
+        api.delete(self.profile_uuid, parcel, self.parcel_uuid)
+
+        # Assert
+        mock_endpoint.delete.assert_called_once_with(self.profile_uuid, expected_path, self.parcel_uuid)
+
+    @parameterized.expand(endpoint_mapping.items())
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.endpoints.configuration.feature_profile.sdwan.system.SystemFeatureProfile")
+    def test_get_method_with_valid_arguments(self, parcel, expected_path, mock_endpoint, mock_session):
+        # Arrange
+        api = SystemFeatureProfileAPI(mock_session)
+        api.endpoint = mock_endpoint
+
+        # Act
+        api.get(self.profile_uuid, parcel, self.parcel_uuid)
+
+        # Assert
+        mock_endpoint.get_by_id.assert_called_once_with(self.profile_uuid, expected_path, self.parcel_uuid)
+
+    @parameterized.expand(endpoint_mapping.items())
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.endpoints.configuration.feature_profile.sdwan.system.SystemFeatureProfile")
+    def test_get_all_method_with_valid_arguments(self, parcel, expected_path, mock_endpoint, mock_session):
+        # Arrange
+        api = SystemFeatureProfileAPI(mock_session)
+        api.endpoint = mock_endpoint
+
+        # Act
+        api.get(self.profile_uuid, parcel)
+
+        # Assert
+        mock_endpoint.get_all.assert_called_once_with(self.profile_uuid, expected_path)
