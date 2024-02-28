31420fa176f875e4927b6ee10a92dcbdc2a26cd4
Jakub Krajewski <jakrajew@cisco.com>
2024-02-28 13:52:14 +0100

    Add Banner Model

--- catalystwan/models/configuration/feature_profile/sdwan/system/banner.py
+++ catalystwan/models/configuration/feature_profile/sdwan/system/banner.py
@@ -1,9 +1,17 @@
-from typing import Literal
+from __future__ import annotations
 
-from pydantic import Field
+from typing import Literal, Union
 
-from catalystwan.api.configuration_groups.parcel import _ParcelBase
+from pydantic import ConfigDict, Field
+
+from catalystwan.api.configuration_groups.parcel import Default, Global, Variable, _ParcelBase
 
 
 class BannerParcel(_ParcelBase):
     type_: Literal["banner"] = Field(default="banner", exclude=True)
+
+    model_config = ConfigDict(
+        extra="forbid",
+    )
+    login: Union[Variable, Global[str], Default[Literal[""]]] = Field(default="")
+    motd: Union[Variable, Global[str], Default[Literal[""]]] = Field(default="")
