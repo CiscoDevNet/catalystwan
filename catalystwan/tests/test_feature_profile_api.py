91d5cc8776c94f7053579845b4134be1e70f7aae
Jakub Krajewski <jakrajew@cisco.com>
2024-02-28 12:01:52 +0100

    Add tests for POST and PUT

--- catalystwan/tests/test_feature_profile_api.py
+++ catalystwan/tests/test_feature_profile_api.py
@@ -80,3 +80,31 @@ class TestSystemFeatureProfileAPI(unittest.TestCase):
 
         # Assert
         mock_endpoint.get_all.assert_called_once_with(self.profile_uuid, expected_path)
+
+    @parameterized.expand(endpoint_mapping.items())
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.endpoints.configuration.feature_profile.sdwan.system.SystemFeatureProfile")
+    def test_create_method_with_valid_arguments(self, parcel, expected_path, mock_endpoint, mock_session):
+        # Arrange
+        api = SystemFeatureProfileAPI(mock_session)
+        api.endpoint = mock_endpoint
+
+        # Act
+        api.create(self.profile_uuid, parcel)
+
+        # Assert
+        mock_endpoint.create.assert_called_once_with(self.profile_uuid, expected_path, parcel)
+
+    @parameterized.expand(endpoint_mapping.items())
+    @patch("catalystwan.session.ManagerSession")
+    @patch("catalystwan.endpoints.configuration.feature_profile.sdwan.system.SystemFeatureProfile")
+    def test_update_method_with_valid_arguments(self, parcel, expected_path, mock_endpoint, mock_session):
+        # Arrange
+        api = SystemFeatureProfileAPI(mock_session)
+        api.endpoint = mock_endpoint
+
+        # Act
+        api.update(self.profile_uuid, parcel, self.parcel_uuid)
+
+        # Assert
+        mock_endpoint.update.assert_called_once_with(self.profile_uuid, expected_path, self.parcel_uuid, parcel)
