2fc573f8a42b77cb4a272bd549b7ebe2069dd4e8
Jakub Krajewski <jakrajew@cisco.com>
2024-03-06 13:08:13 +0100

    Add banner, add more template types, work on Basic

---
+++ catalystwan/utils/config_migration/converters/feature_template/banner.py
@@ -0,0 +1,15 @@
+from catalystwan.models.configuration.feature_profile.sdwan.system import BannerParcel
+
+
+class BannerTemplateConverter:
+    @staticmethod
+    def create_parcel(name: str, description: str, template_values: dict) -> BannerParcel:
+        """
+        Creates an AAA object based on the provided template values.
+
+        Returns:
+            AAA: An AAA object with the provided template values.
+        """
+        template_values["name"] = name
+        template_values["description"] = description
+        return BannerParcel(**template_values)
