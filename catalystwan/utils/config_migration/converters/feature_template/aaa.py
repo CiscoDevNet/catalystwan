e159fb9aa79ca050ffa7a8cf48935006ff5de35b
sbasan <sbasan@cisco.com>
2024-03-06 13:41:51 +0100

    transform fixes

--- catalystwan/utils/config_migration/converters/feature_template/aaa.py
+++ catalystwan/utils/config_migration/converters/feature_template/aaa.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from catalystwan.api.configuration_groups.parcel import Global
 from catalystwan.models.configuration.feature_profile.sdwan.system import AAAParcel
 
 
@@ -22,6 +25,36 @@ class AAATemplateConverter:
             "port",
             "cts_auth_list",
         )
+
+        if template_values.get("server_auth_order") is not None:
+            global_object = template_values["server_auth_order"]
+            template_values["server_auth_order"] = Global[List[str]](value=global_object.value.split(","))
+
+        if template_values.get("radius") is not None:
+            radius_list = template_values["radius"]
+            for radius in radius_list:
+                servers = radius.get("server", {})
+                for server in servers:
+                    key_enum = server.get("key_enum")
+                    server["key_enum"] = Global[str](value=str(key_enum.value))
+
+        if template_values.get("tacacs") is not None:
+            tacacs_list = template_values["tacacs"]
+            for tacacs in tacacs_list:
+                servers = tacacs.get("server", {})
+                for server in servers:
+                    key_enum = server.get("key_enum")
+                    server["key_enum"] = Global[str](value=str(key_enum.value))
+
+        if template_values.get("accounting_rule") is not None:
+            accounting_rule = template_values["accounting_rule"]
+            for rule_item in accounting_rule:
+                rule_item["group"] = Global[List[str]](value=rule_item["group"].value.split(","))
+
+        if template_values.get("authorization_rule") is not None:
+            authorization_rule = template_values["authorization_rule"]
+            for rule_item in authorization_rule:
+                rule_item["group"] = Global[List[str]](value=rule_item["group"].value.split(","))
 
         for prop in delete_properties:
             if template_values.get(prop) is not None:
